b8a6eff37263789738299bbeafc14e70a6c5f6cf
lehyaric <lehyaric@localhost>
2004-06-23 14:04:06 +0200

    Regression tests are currently being checked. There seems to be a problem in examples++-tutorial on Hydre.

--- regtests.pl
+++ regtests.pl
@@ -9,7 +9,12 @@ use strict;
 
 # All existing FreeFem++ versions for this platform
 my @progs;
-push @progs,"$ENV{PROGLOC}/std/FreeFem++$ENV{EXEEXT}";
+
+# At present, we do not test the standard version because its window
+# is too invasive (at least under X11).
+
+#push @progs,"$ENV{PROGLOC}/std/FreeFem++$ENV{EXEEXT}";
+
 push @progs,"$ENV{PROGLOC}/nw/FreeFem++-nw$ENV{EXEEXT}";
 push @progs,"$ENV{PROGLOC}/x11/FreeFem++-x11$ENV{EXEEXT}"
   if $ENV{X11PROG} ne "";
@@ -17,13 +22,20 @@ push @progs,"$ENV{PROGLOC}/glx/FreeFem++-glx$ENV{EXEEXT}"
   if $ENV{GLXPROG} ne "";
 push @progs,"$ENV{PROGLOC}/agl/FreeFem++-agl$ENV{EXEEXT}"
   if $ENV{AGLPROG} ne "";
+push @progs,"$ENV{PROGLOC}/mpi/FreeFem++-mpi$ENV{EXEEXT}"
+  if $ENV{MPIPROG} ne "";
 
 # Loop on all available FreeFem programs
+my $count=0;
 foreach my $ff (@progs) {
+  $count++;
 
-  print "checking $ff\n";
-
-  # Run FreeFem++
-  system "$ff all.edp > regtests.log";
+  # Running FreeFem++ on regtests.edp (specific to regression tests),
+  # otherwise on all.edp.
+  my $test="all.edp";
+  $test="regtests.edp" if -f "regtests.edp";
+  my $cmd="$ff $test > regtests-$count.log";
+  print "$cmd\n";
+  system $cmd;
   die if $?;
 }
