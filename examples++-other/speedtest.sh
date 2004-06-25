d253af323a16f1777083e177a6fb17acfdf58a11
lehyaric <lehyaric@localhost>
2004-06-25 10:55:37 +0200

    - Restructuring the regression tests suite to run all non windowed tests in one go. - New configuration file "config-version.h" that can be included separately into MacOS 9 sources.

---
+++ examples++-other/speedtest.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Speed benchmark for FreeFem++
+# $Id$
+
+# The FreeFem++ executable
+ffexe=${PROGLOC}/nw/FreeFem++-nw${EXEEXT}
+if test ! -x $ffexe
+then
+    echo $ffexe non existent
+    exit 1
+fi
+
+# Write some build information into the trace file
+echo ------------------------------------------- >> speedtest.out
+date >> speedtest.out
+echo $CXXFLAGS >> speedtest.out
+
+# Run the actual test
+$ffexe lap3-cpu.edp	\
+		|grep -E --					\
+		'-- lap (cholesky|CG|UMFPACK) +[0-9]+x[0-9]+'	\
+		|tee speedtest.out
