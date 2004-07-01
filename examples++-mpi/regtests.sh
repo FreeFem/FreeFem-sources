c042773038ab7b0e742c02a79dbf7a29545576da
lehyaric <lehyaric@localhost>
2004-07-01 13:21:41 +0200

    More tests on the parallel version (FreeFem++-mpi)

--- examples++-mpi/regtests.sh
+++ examples++-mpi/regtests.sh
@@ -2,29 +2,23 @@
 # Testing procedure for MPI version of FreeFem++
 # $Id$
 
-# The MPI executable
-ffexe=${PROGLOC}/mpi/FreeFem++-mpi${EXEEXT}
-
-# Doing one parallel test
-function dotest(){
-    local cmd="mpirun -np 2 -machinefile machinefile $ffexe $1"
-    echo $cmd
-    eval $cmd
-    if test $? != 0
-	then
-	exit 1
-    fi
-}
-
-# Only if the executable was built
-if test "$MPIPROG" != ""
-then
-    host=`hostname`
-    echo $host>machinefile
-    echo $host>>machinefile
-
-    dotest essai.edp
-    dotest schwarz.edp
-    dotest schwarz-b.edp
-    dotest schwarz-c.edp
+NPROCS=2 REGTESTS=essai.edp ../regtests.sh
+if test $? != 0
+    then
+    exit 1
+fi
+NPROCS=2 REGTESTS=schwarz.edp ../regtests.sh
+if test $? != 0
+    then
+    exit 1
+fi
+NPROCS=2 REGTESTS=schwarz-b.edp ../regtests.sh
+if test $? != 0
+    then
+    exit 1
+fi
+NPROCS=2 REGTESTS=schwarz-c.edp ../regtests.sh
+if test $? != 0
+    then
+    exit 1
 fi
