e8fe9f2a171bb02d923ec835d973e9ca69f99fb0
lehyaric <lehyaric@localhost>
2004-07-01 14:26:01 +0200

    More updates to make parallel regression tests work.

--- examples++-mpi/regtests.sh
+++ examples++-mpi/regtests.sh
@@ -2,23 +2,24 @@
 # Testing procedure for MPI version of FreeFem++
 # $Id$
 
-NPROCS=2 REGTESTS=essai.edp ../regtests.sh
+NPROCS=2 REGEDP=essai.edp ../regtests.sh
 if test $? != 0
     then
     exit 1
 fi
-NPROCS=2 REGTESTS=schwarz.edp ../regtests.sh
+NPROCS=2 REGEDP=schwarz.edp ../regtests.sh
 if test $? != 0
     then
     exit 1
 fi
-NPROCS=2 REGTESTS=schwarz-b.edp ../regtests.sh
+NPROCS=2 REGEDP=schwarz-b.edp ../regtests.sh
 if test $? != 0
     then
     exit 1
 fi
-NPROCS=2 REGTESTS=schwarz-c.edp ../regtests.sh
+NPROCS=2 REGEDP=schwarz-c.edp ../regtests.sh
 if test $? != 0
     then
     exit 1
 fi
+
