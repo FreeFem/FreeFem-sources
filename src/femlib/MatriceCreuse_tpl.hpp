72708faf1b90a992d204a9ec1dc2566c90df9681
hecht <hecht@localhost>
2004-09-13 15:27:09 +0200

    C++ encapsulte cblas.h in src/femlib/MatriceCreuse_tpl.hpp for freebsd OS

--- src/femlib/MatriceCreuse_tpl.hpp
+++ src/femlib/MatriceCreuse_tpl.hpp
@@ -11,7 +11,9 @@
 //  on MacOS9 under MWERKS
 //  cblas_ddot macos-9 is not 
 #ifdef HAVE_CBLAS_H
+extern "C" {
 #include <cblas.h> 
+}
 #define WITHBLAS 1
 #elif HAVE_VECLIB_CBLAS_H
 #include <vecLib/cblas.h> 
