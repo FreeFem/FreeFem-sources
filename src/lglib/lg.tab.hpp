ee06b875e53900075f26c899df5c1cb4a06f2639
lehyaric <lehyaric@localhost>
2004-10-08 12:21:58 +0200

    First model of an Integrated Development Environment for FreeFem++. The main window layout is already in place.

---
+++ src/lglib/lg.tab.hpp
@@ -0,0 +1,149 @@
+/* A Bison parser, made by GNU Bison 1.875a.  */
+
+/* Skeleton parser for Yacc-like parsing with Bison,
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+/* As a special exception, when this file is copied by Bison into a
+   Bison output file, you may use that output file without restriction.
+   This special exception was added by the Free Software Foundation
+   in version 1.24 of Bison.  */
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     IF = 258,
+     ELSE = 259,
+     SET = 260,
+     GTGT = 261,
+     LTLT = 262,
+     OR = 263,
+     AND = 264,
+     NE = 265,
+     EQ = 266,
+     GE = 267,
+     LE = 268,
+     DOTSLASH = 269,
+     DOTSTAR = 270,
+     MOINSMOINS = 271,
+     PLUSPLUS = 272,
+     UNARY = 273,
+     LNUM = 274,
+     DNUM = 275,
+     CNUM = 276,
+     ID = 277,
+     FESPACEID = 278,
+     IDPARAM = 279,
+     STRING = 280,
+     ENDOFFILE = 281,
+     INCLUDE = 282,
+     LOAD = 283,
+     BIDON = 284,
+     FOR = 285,
+     WHILE = 286,
+     BREAK = 287,
+     CONTINUE = 288,
+     RETURN = 289,
+     TYPE = 290,
+     FUNCTION = 291,
+     FESPACE = 292,
+     PLUSEQ = 293,
+     MOINSEQ = 294,
+     MULEQ = 295,
+     DIVEQ = 296,
+     ARROW = 297,
+     BORDER = 298,
+     CURVE = 299,
+     SOLVE = 300
+   };
+#endif
+#define IF 258
+#define ELSE 259
+#define SET 260
+#define GTGT 261
+#define LTLT 262
+#define OR 263
+#define AND 264
+#define NE 265
+#define EQ 266
+#define GE 267
+#define LE 268
+#define DOTSLASH 269
+#define DOTSTAR 270
+#define MOINSMOINS 271
+#define PLUSPLUS 272
+#define UNARY 273
+#define LNUM 274
+#define DNUM 275
+#define CNUM 276
+#define ID 277
+#define FESPACEID 278
+#define IDPARAM 279
+#define STRING 280
+#define ENDOFFILE 281
+#define INCLUDE 282
+#define LOAD 283
+#define BIDON 284
+#define FOR 285
+#define WHILE 286
+#define BREAK 287
+#define CONTINUE 288
+#define RETURN 289
+#define TYPE 290
+#define FUNCTION 291
+#define FESPACE 292
+#define PLUSEQ 293
+#define MOINSEQ 294
+#define MULEQ 295
+#define DIVEQ 296
+#define ARROW 297
+#define BORDER 298
+#define CURVE 299
+#define SOLVE 300
+
+
+
+
+#if ! defined (YYSTYPE) && ! defined (YYSTYPE_IS_DECLARED)
+#line 80 "lg.y"
+typedef union YYSTYPE { 
+ double dnum;
+ long lnum;
+ char * str;
+ char oper[8];
+ CC_F0 cexp;
+ Routine   *routine;
+ AC_F0 args;
+ aType type;
+ CListOfInst cinst;
+ Block * block; 
+ ListOfId *clist_id;
+} YYSTYPE;
+/* Line 1240 of yacc.c.  */
+#line 141 "lg.tab.hpp"
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+
+extern YYSTYPE lglval;
+
+
+
