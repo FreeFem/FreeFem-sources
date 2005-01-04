359e4edf38aa652276d8a092e9dee329f05f7b22
hecht <frederic.hecht@upmc.fr>
2005-01-04 16:45:19 +0100

    add qf1pElump   new quadrature formular in int1d() ...

--- src/fflib/lgfem.cpp
+++ src/fflib/lgfem.cpp
@@ -3507,6 +3507,7 @@ void  init_lgfem()
  Global.New("qf1pE",CConstant<const QuadratureFormular1d *>(&QF_GaussLegendre1));
  Global.New("qf2pE",CConstant<const QuadratureFormular1d *>(&QF_GaussLegendre2));
  Global.New("qf3pE",CConstant<const QuadratureFormular1d *>(&QF_GaussLegendre3));
+ Global.New("qf1pElump",CConstant<const QuadratureFormular1d *>(&QF_LumpP1_1D));
  
  //  juste du code genere 
  
