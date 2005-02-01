923b19e1f647e0814fc3a5b01491683ab59535d3
lehyaric <lehyaric@localhost>
2005-02-01 16:49:04 +0100

    New script to remove generated files (in order to prevent CVS conflicts)

---
+++ cleanregen.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -x
+# Clean files that can be regenerated, to avoid CVS conflicts
+
+# Antoine Le Hyaric - LJLL Paris 6 - lehyaric@ann.jussieu.fr
+# $Id$
+
+find . -name Makefile.in -exec rm {} \;
+rm configure
+rm src/fflib/strversionnumber.cpp
+rm examples++-tutorial/StokesUzawa.edp
+rm config.h.in
