aba5664a9de737eeae8906b17e3fd451d5ea3c1a
hecht <hecht@localhost>
2005-09-27 22:35:41 +0200

    correct load.cpp to change suffix and prefix

--- src/fflib/load.cpp
+++ src/fflib/load.cpp
@@ -15,33 +15,57 @@ using namespace std;
 #elif WIN32
 #include <windows.h>
 #endif
-bool load(string s)
+bool load(string ss)
 {
   bool ret=false;
   void * handle = 0;
+  const int nbprefix=2,nbsuffix=2;
+  string  prefix[nbprefix];
+  string suffix[nbsuffix] ;
+  
+  prefix[0]="";
+  prefix[1]="./";
+  
+  suffix[0]="";
+  suffix[1]=".so";
+#ifdef  _APPLE_
+  suffix[1]=".dylib";
+#endif  
+#ifdef WIN32  
+  suffix[1]=".dll";
+#endif 
+ int i,j; 
+ for ( i= 0;i< nbprefix;i++)
+ for ( j= 0;j< nbsuffix;j++)
+  {
+    string s= prefix[i]+ss+suffix[i];
 #ifdef LOAD  
   handle = dlopen (s.c_str(), RTLD_LAZY ); 
   ret= handle !=0;
-  if ( handle == 0 ) 
-    cerr  <<   "load: " <<s << "\n \t fail : " << dlerror() << endl;
-  else 
-    cout << "load: dlopen(" << s << ") = " << handle << endl;
 #elif WIN32
   {
    HINSTANCE mod=  LoadLibrary(s.c_str());
    if(mod==0) 
      {
        DWORD merr = GetLastError();
-    cerr  <<   "loadLibary : " <<s << "\n \t fail : " << merr << endl;
+       cerr  <<   "\n try loadLibary : " <<s << "\n \t fail : " << merr << endl;
      }
   else 
-    cout << "load: LoadLibrary (" << s << ") = " << mod << endl;
-
+    cout << "\nload: LoadLibrary (" << s << ") = " << mod << endl;
+   
   }
 #else
-  cout << "load: sorry no dlopen on this system " << s << " \n" ;
+  cout << "------------------------------------   \n" ;
+  cout << "  load: sorry no dlopen on this system " << s << " \n" ;
+  cout << "------------------------------------   \n" ;
+  
 #endif  
- 
+ }
+  if ( ! ret ) 
+    cerr  <<   "load error : [" << prefix[1]<<"]" <<ss <<"[" << suffix[1]<<"]"<< "\n \t fail : "  << endl;
+  else 
+    cout << "\nload: dlopen(" <<prefix[i] << ss << suffix[j] << ") = " << handle << endl;
+  
   return handle ;
 }
 
