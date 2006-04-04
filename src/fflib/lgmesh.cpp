153b11f00fa102a9045ca5a904e7894961a389a2
hecht <frederic.hecht@upmc.fr>
2006-04-04 11:25:56 +0200

    coorect consistance beetween movmesh and checkmovemesh

--- src/fflib/lgmesh.cpp
+++ src/fflib/lgmesh.cpp
@@ -865,7 +865,8 @@ Mesh * MoveTheMesh(const Fem2D::Mesh &Th,const KN_<double> & U,const KN_<double>
     {
       int i0=Th(i,0), i1=Th(i,1),i2=Th(i,2);
       R a= Area2(v[i0],v[i1],v[i2])/2;
-      if ( a < Th[i].area/100 ) 
+      if ( a < Th[i].area*1.e-6
+       ) 
        { nberr++;
         if (verbosity>1) 
          {
