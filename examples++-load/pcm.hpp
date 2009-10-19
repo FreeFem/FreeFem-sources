594731f9650968db69134ec20a6cc27625b39505
hecht <hecht@localhost>
2009-10-19 22:46:57 +0200

    add tools to read pcm opticflow pcm.cpp pcm.hpp pcm2rnm.cpp

---
+++ examples++-load/pcm.hpp
@@ -0,0 +1,43 @@
+
+/*
+ * pcm.h
+ *
+ * Author: Dion Crannitch (adapted from Dave Mason's complexMap class)
+ * Last Modified : 22 June 1999
+ *
+ * Declarations for PCM class, a data structure containing a 2D vector
+ * image, with encapsulated operations for reading and writing PCM files.
+ *
+ */
+
+#ifndef __PCM__
+#define __PCM__
+
+#include <cstdio>
+using namespace std;
+typedef struct { 
+  float r,i;
+} pcm_complex;
+
+class PCM {
+  void CalcMax();
+ public:
+  int  width,height;
+  unsigned long pixels;
+  float max;
+  pcm_complex *image; 
+  PCM(int w,int h);
+  PCM(const char *filename);
+  pcm_complex *Get(int x, int y);
+  void Set(int x, int y, pcm_complex c);
+  void Load(const char *filename);
+  void Save(const char *filename);
+  ~PCM();
+private:
+    PCM (const PCM &);
+    void operator=(const PCM &);
+};
+
+#endif
+
+
