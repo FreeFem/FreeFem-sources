90b471686e0ee001969c5852c287c44c64132ad2
hecht <frederic.hecht@upmc.fr>
2010-04-23 18:32:39 +0200

    *** empty log message ***

--- src/solver/complex_SuperLU_DIST_FreeFem.cpp
+++ src/solver/complex_SuperLU_DIST_FreeFem.cpp
@@ -1,5 +1,5 @@
 //   for automatic  compilation with ff-c++
-//ff-c++-LIBRARY-dep: superlu_dist  blas 
+//ff-c++-LIBRARY-dep: superlu_dist  blas parmetis
 //ff-c++-cpp-dep: 
 // ORIG-DATE: 02/2009
 // -*- Mode : c++ -*-
