aea27966cb3c780ec28c9cf93b4732686f732991
hecht <frederic.hecht@upmc.fr>
2010-04-29 23:13:49 +0200

    update configure.ac for add MPIRUN

--- regtests.sh
+++ regtests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Runs all regression tests on all compiled FreeFem++ versions
 # ------------------------------------------------------------
-
+MPIRUN=`awk  '$1 =="MPIRUN" {print $3}' Makefile`
 # Antoine Le Hyaric - LJLL Paris 6 - lehyaric@ann.jussieu.fr - 19/5/04
 # $Id$
 
@@ -69,15 +69,15 @@ fi
 
 if test "${MPIPROG}" != ""
     then
-    mpich=`mpirun -h 2>&1 |grep mpich  |wc  -l`
+    mpich=`${MPIRUN=mpirun} -h 2>&1 |grep mpich  |wc  -l`
     host=`hostname`
     echo $host>machinefile
     echo $host>>machinefile
     if [ $mpich -ne 0 ] ; then
-    dotest "mpirun -np $nprocs -machinefile machinefile ${PROGLOC}/mpi/FreeFem++-mpi${EXEEXT}" mpi $script
+    dotest "${MPIRUN} -np $nprocs -machinefile machinefile ${PROGLOC}/mpi/FreeFem++-mpi${EXEEXT}" mpi $script
     else
 	[[ -f "$(which lamboot 2>/dev/null)" ]] && lamboot	
-     dotest "mpirun -np $nprocs ${PROGLOC}/mpi/FreeFem++-mpi${EXEEXT}" mpi $script
+     dotest "${MPIRUN} -np $nprocs ${PROGLOC}/mpi/FreeFem++-mpi${EXEEXT}" mpi $script
     fi
 fi
 
