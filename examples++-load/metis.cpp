125bf7d4db8f156fa9f4ee6ce46b78025d25a69e
Frederic Hecht <frederic.hecht@upmc.fr>
2010-11-16 19:53:30 +0100

    romve cout in metis.cpp

--- examples++-load/metis.cpp
+++ examples++-load/metis.cpp
@@ -39,7 +39,8 @@ KN<long> * partmetis(Stack s,KN<long> * const & part,Mesh * const & pTh,long con
      METIS_PartMeshNodal(&nt, &nv, elmnts, &etype , &numflag, &nparts, &edgecut, epart, npart);
     else
      METIS_PartMeshDual(&nt, &nv, elmnts, &etype , &numflag, &nparts, &edgecut, epart, npart);
-    printf("  %d-way Edge-Cut: %7d, Balance: %5.2f\n", nparts, nve, ComputeElementBalance(nt, nparts, epart));
+    if(verbosity)
+      printf("  --metis: %d-way Edge-Cut: %7d, Balance: %5.2f Nodal=0/Dual %d\n", nparts, nve, ComputeElementBalance(nt, nparts, epart,NO));
     part->resize(nt);
     *part=epart;
     return part;
