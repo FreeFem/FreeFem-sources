936e5d5d9037a36f863f46dd9768c3c306229a30
Frederic Hecht <frederic.hecht@upmc.fr>
2010-11-18 16:11:24 +0100

    coorect makefile for ff-mpirun and add srandomdev by read /dev/random + time

--- examples++-load/ffrandom.cpp
+++ examples++-load/ffrandom.cpp
@@ -8,9 +8,36 @@
 using namespace std;
 #include "error.hpp"
 #include "AFunction.hpp"
-#include <cstdlib>
 
-#ifdef HAVE_SRANDOMDEV
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <ctime>
+
+unsigned long good_seed()
+{
+  unsigned long random_seed,  random_seed_a,random_seed_b;
+  std::ifstream file ("/dev/random", std::ios::binary);
+  if (file.is_open())
+    {
+      unsigned long memblock[10];
+      size_t size = sizeof(int);
+      file.read ((char*) (void *) memblock, size);
+      file.close();
+      random_seed_a = memblock[0];
+    }// end if
+  else
+    {
+      random_seed_a = 0;
+    }
+  random_seed_b = std::time(0);
+  random_seed = random_seed_a xor random_seed_b;
+  if(verbosity>1)
+  cout << " good_seed =" << random_seed << endl;
+  return random_seed;
+} // end good_seed()
+
+
 template<class R>
 class  OneOperator_0 : public OneOperator {
   class E_F0_F :public  E_F0mps { public:
@@ -31,7 +58,14 @@ public:
 };
 
 long ffsrandom(long  s) { srandom( (unsigned int ) s); return 0;}
-long ffsrandomdev() { srandomdev(); return 0;}
+long ffsrandomdev() { 
+#ifdef HAVE_SRANDOMDEV
+  srandomdev(); 
+#else
+  srandom(good_seed());
+#endif
+
+return 0;}
 
 class Init { public:
   Init();
@@ -47,4 +81,4 @@ Init::Init(){
 }
  
 /* These real versions are due to Isaku Wada, 2002/01/09 added */
-#endif
+
