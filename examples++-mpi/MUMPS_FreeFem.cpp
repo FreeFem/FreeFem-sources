c68b2236708761b3899b29b106813b17e909f6da
Frederic Hecht <frederic.hecht@upmc.fr>
2010-11-30 15:47:39 +0100

    do coorect of periodic boundary condition + MUMPS link order

--- examples++-mpi/MUMPS_FreeFem.cpp
+++ examples++-mpi/MUMPS_FreeFem.cpp
@@ -7,7 +7,7 @@
 // AUTHOR   : Jacques Morice
 // E-MAIL   : jacques.morice@ann.jussieu.fr
 //
-//ff-c++-LIBRARY-dep: metis mumps  blas ptscotch blacs scalapack mpifc  fc  
+//ff-c++-LIBRARY-dep:  mumps parmetis ptscotch  scalapack blacs blas  mpifc  fc  
 //ff-c++-cpp-dep: 
 
 /* 
