5adc886935c33f68372215c69cd3dc0b13a94b89
Frederic Hecht <frederic.hecht@upmc.fr>
2012-02-24 17:43:14 +0100

    add ipopt download

--- src/femlib/RNM.hpp
+++ src/femlib/RNM.hpp
@@ -1123,7 +1123,8 @@ class KN :public KN_<R> { public:
 //          { return   (KN<const_R> &) *this;}
 //    operator KN<const_R> const & ()  const 
 //          { return (const KN<const_R>& ) *this;}
-  void init(long nn) {this->n=nn;this->step=1;this->next=-1;this->v=new R[nn];}
+    static void fill0(R *v,int n) { if(n && v) for(int i=0;i<n;++i) v[i]=R();} 
+    void init(long nn) {this->n=nn;this->step=1;this->next=-1;this->v=new R[nn];fill0(this->v,this->n) ;}
   void init() {this->n=0;this->step=1;this->next=-1;this->v=0;}
   void init(const KN_<R> & a){init(a.N()); operator=(a);}
   void resize(long nn) {
