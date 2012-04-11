11c3c10d00ee9a941c39eeec502d06454a3f4c53
Frederic Hecht <frederic.hecht@upmc.fr>
2012-04-11 22:46:11 +0200

    update doc with  isoline example

--- examples++-load/dfft.cpp
+++ examples++-load/dfft.cpp
@@ -10,6 +10,8 @@
 #include "AFunction_ext.hpp"
 #include <fftw3.h>
 
+
+template<class Complex>
 class DFFT_1d2dor3d
 {  
 public: 
@@ -28,26 +30,26 @@ public:
   
 };
 
-DFFT_1d2dor3d dfft(KN<Complex> * const  & x,const long &sign)
+DFFT_1d2dor3d<Complex>  dfft(KN<Complex> * const  & x,const long &sign)
 {
-  return DFFT_1d2dor3d(x,sign);
+  return DFFT_1d2dor3d<Complex>(x,sign);
 }
 
-DFFT_1d2dor3d dfft(KN<Complex> *const  &x,const long &nn,const long &sign)
+DFFT_1d2dor3d<Complex>  dfft(KN<Complex> *const  &x,const long &nn,const long &sign)
 {
-  return DFFT_1d2dor3d(x,sign,nn);
+  return DFFT_1d2dor3d<Complex>(x,sign,nn);
 }
-DFFT_1d2dor3d dfft(KN<Complex> *const  &x,const long &nn,const long &kk,const long &sign)
+DFFT_1d2dor3d<Complex>  dfft(KN<Complex> *const  &x,const long &nn,const long &kk,const long &sign)
 {
-    return DFFT_1d2dor3d(x,sign,nn,kk);
-}
-
-DFFT_1d2dor3d dfft(KNM<Complex> * const &  x,const long &sign)
-{
-  return DFFT_1d2dor3d(x,sign);
+  return DFFT_1d2dor3d<Complex>(x,sign,nn,kk);
 }
 
-KN<Complex> * dfft_eq(  KN<Complex> * const   &x,const DFFT_1d2dor3d & d)
+DFFT_1d2dor3d<Complex> dfft(KNM<Complex> * const &  x,const long &sign)
+{
+  return DFFT_1d2dor3d<Complex>(x,sign);
+}
+
+KN<Complex> * dfft_eq(  KN<Complex> * const   &x,const DFFT_1d2dor3d<Complex>  & d)
 {
   ffassert(x->N()==d.n*d.m*d.k );
   Complex *px =  *x;
@@ -76,18 +78,63 @@ KN<Complex> * dfft_eq(  KN<Complex> * const   &x,const DFFT_1d2dor3d & d)
   return  x;
 }
 
+
+KN<double> * dfft_eq(  KN<double> * const   &x,const DFFT_1d2dor3d<double>  & d)
+{
+  ffassert(0); 
+  /*
+  ffassert(x->N()==d.n*d.m*d.k );
+  double *px =  *x;
+  fftw_plan p; 
+  //cout << " dfft " << px << " = " << d.x << " n = " << d.n << " " << d.m << " sign = " << d.sign << endl; 
+  if ( d.k ==1)
+  {
+   if ( d.n > 1)
+    p = fftw_plan_dft_r2r_2d(d.n,d.m,reinterpret_cast<double*>(d.x),reinterpret_cast<double*> (px),d.sign,FFTW_ESTIMATE);
+   else
+    p = fftw_plan_dft_r2r_1d(d.m ,reinterpret_cast<double*>(d.x),reinterpret_cast<double*> (px),d.sign,FFTW_ESTIMATE);
+  }
+   else 
+   {
+       if ( d.n > 1)
+           p = fftw_plan_dft_r2r_3d(d.n,d.m,d.k,reinterpret_cast<double*>(d.x),reinterpret_cast<double*> (px),d.sign,FFTW_ESTIMATE);
+       else
+           p = fftw_plan_dft_r2r_2d(d.m,d.k,reinterpret_cast<double*>(d.x),reinterpret_cast<double*> (px),d.sign,FFTW_ESTIMATE);
+       
+   }
+ // cout << " ---" ;
+  fftw_execute(p);
+ // cout << " ---" ;
+  fftw_destroy_plan(p);
+ // cout << " ---" ;
+ */
+  return  x;
+}
+
 class Init { public:
   Init();
 };
 LOADINIT(Init);
 Init::Init(){
+  typedef DFFT_1d2dor3d<Complex>  DFFT_C;
+  typedef DFFT_1d2dor3d<double>  DFFT_R;
+
   cout << " lood: init dfft " << endl;
-  Dcl_Type<DFFT_1d2dor3d >();
-  Global.Add("dfft","(", new OneOperator2_<DFFT_1d2dor3d,KN<Complex>*,long >(dfft ));
-  Global.Add("dfft","(", new OneOperator3_<DFFT_1d2dor3d,KN<Complex>*,long,long >(dfft ));
-  Global.Add("dfft","(", new OneOperator4_<DFFT_1d2dor3d,KN<Complex>*,long,long, long >(dfft ));
-  Global.Add("dfft","(", new OneOperator2_<DFFT_1d2dor3d,KNM<Complex>*,long >(dfft ));
-  TheOperators->Add("=", new OneOperator2_<KN<Complex>*,KN<Complex>*,DFFT_1d2dor3d>(dfft_eq));
+  Dcl_Type<DFFT_C >();
+  Dcl_Type<DFFT_R >();
+
+  Global.Add("dfft","(", new OneOperator2_<DFFT_C,KN<Complex>*,long >(dfft ));
+  Global.Add("dfft","(", new OneOperator3_<DFFT_C,KN<Complex>*,long,long >(dfft ));
+  Global.Add("dfft","(", new OneOperator4_<DFFT_C,KN<Complex>*,long,long, long >(dfft ));
+  Global.Add("dfft","(", new OneOperator2_<DFFT_C,KNM<Complex>*,long >(dfft ));
+  TheOperators->Add("=", new OneOperator2_<KN<Complex>*,KN<Complex>*,DFFT_C>(dfft_eq));
+  /*
+  Global.Add("dfft","(", new OneOperator2_<DFFT_R,KN<double>*,long >(dfft ));
+  Global.Add("dfft","(", new OneOperator3_<DFFT_R,KN<double>*,long,long >(dfft ));
+  Global.Add("dfft","(", new OneOperator4_<DFFT_R,KN<double>*,long,long, long >(dfft ));
+  Global.Add("dfft","(", new OneOperator2_<DFFT_R,KNM<double>*,long >(dfft ));
+  TheOperators->Add("=", new OneOperator2_<KN<double>*,KN<double>*,DFFT_R>(dfft_eq));
+  */
   // TheOperators->Add("=", new OneOperator2_<KNM<Complex>*,KNM<Complex>*,DFFT_1d2dor3d>(dfft_eq));
 
 }
