7661467cd67f04a44394dac560a7fc4be8fc5693
Frederic Hecht <frederic.hecht@upmc.fr>
2012-10-03 14:38:28 +0200

    add find root of real poly in  gsl plugin

--- examples++-load/gsl.cpp
+++ examples++-load/gsl.cpp
@@ -35,14 +35,42 @@
 #include <gsl/gsl_sf_transport.h>
 #include <gsl/gsl_sf_trig.h>
 #include <gsl/gsl_sf_zeta.h>
+#include <gsl/gsl_poly.h>
 
 #include "ff_gsl_awk.hpp"
 
+long gslpolysolvequadratic( KN_<double> a,  KN_<double> x)
+{
+  ffassert(a.N()>2 && x.N()>1);
+  return gsl_poly_solve_quadratic (a[2],a[1],a[0],&(x[0]),&(x[1]));
+  }
+long gslpolysolvecubic( KN_<double> a,  KN_<double> x)
+{
+  ffassert(a.N()>2 && x.N()>2);
+  return gsl_poly_solve_cubic (a[2],a[1],a[0],&(x[0]),&(x[1]),&(x[2]));
+  }
+
+long gslpolycomplexsolve( KN_<double> a,  KN_<Complex> x)
+{
+  int n = a.N();
+  ffassert( n-1 <=  x.N()); 
+  KN<double> z(n*2); 
+  gsl_poly_complex_workspace * w= gsl_poly_complex_workspace_alloc (n);
+  int ok=gsl_poly_complex_solve (&a[0], n, w, &z[0]);
+  gsl_poly_complex_workspace_free (w);
+  for (long i = 0; i < n-1; i++)
+    x[i] = Complex(z[2*i], z[2*i+1]);
+  return ok; 
+  }
 
 class Init { public:
   Init();
 };
 LOADINIT(Init);
 Init::Init(){
-  init_gsl_sf() ; 
+  init_gsl_sf() ;
+  Global.Add("gslpolysolvequadratic","(",new OneOperator2<long,KN_<double>,KN_<double> >( gslpolysolvequadratic));
+  Global.Add("gslpolysolvecubic","(",new OneOperator2<long,KN_<double>,KN_<double> >(gslpolysolvecubic));
+  Global.Add("gslpolycomplexsolve","(",new OneOperator2<long,KN_<double>,KN_<Complex> >( gslpolycomplexsolve));
+
 }
