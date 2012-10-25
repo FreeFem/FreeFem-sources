8dd6c7a7ac2e2423739718106f12809ae851db56
Frederic Hecht <frederic.hecht@upmc.fr>
2012-10-25 10:03:41 +0200

    remove const in cblas interface sdot mistake  in openblas

--- src/femlib/MatriceCreuse_tpl.hpp
+++ src/femlib/MatriceCreuse_tpl.hpp
@@ -34,7 +34,19 @@ template<class R> inline R blas_sdot(const int n,const R *sx,const int incx,cons
     s += *sx * *sy;   
    return  s;
 }
-#ifdef COMMON_H
+
+template<class R> inline R blas_sdot( int n, R *sx, int incx, R *sy, int  incy)
+{
+    R s=R();
+    
+    if(incx == 1 && incy == 1)
+        for (int k = 0; k< n; k++)
+            s += *sx++ * * sy++;
+    else
+        for (int k = 0; k< n; k++, sx += incx, sy += incy)
+            s += *sx * *sy;
+    return  s;
+}
 // OpenBlas PB with constant  remove const ....
 template<> inline float blas_sdot(const int n,  float *sx, int incx, float *sy, int  incy)
 {
@@ -56,28 +68,7 @@ template<> inline  complex<float> blas_sdot( int n,  complex<float> *sx, int inc
     cblas_zdotu_sub(n,( void *)sx,incx,( void *)sy,incy,( void *)&s);
     return s;
 }
-#else
-template<> inline float blas_sdot(const int n,const  float *sx,const int incx,const float *sy,const int  incy)
-{
-  return cblas_sdot(n,sx,incx,sy,incy);
-}
-template<> inline double blas_sdot(const int n,const  double *sx,const int incx,const double *sy,const int  incy)
-{
-  return cblas_ddot(n,sx,incx,sy,incy);
-}
-template<> inline  complex<double> blas_sdot(const int n,const  complex<double> *sx,const int incx,const complex<double> *sy,const int  incy)
-{
-  complex<double> s;
-   cblas_zdotu_sub(n,(const void *)sx,incx,(const void *)sy,incy,( void *)&s);
-  return s;
-}
-template<> inline  complex<float> blas_sdot(const int n,const  complex<float> *sx,const int incx,const complex<float> *sy,const int  incy)
-{
-  complex<float> s;
-   cblas_zdotu_sub(n,(const void *)sx,incx,(const void *)sy,incy,( void *)&s);
-  return s;
-}
-#endif 
+
 #endif
 //  end modif FH
 using Fem2D::HeapSort;
