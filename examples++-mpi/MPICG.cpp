719cb596d024c6beecd00c3dab252c2549e9382d
Frederic Hecht <frederic.hecht@upmc.fr>
2013-11-20 21:52:07 +0100

    correct typo

--- examples++-mpi/MPICG.cpp
+++ examples++-mpi/MPICG.cpp
@@ -156,7 +156,7 @@ int ConjuguedGradient2(const M & A,const P & C,KN_<R> &x,const KN_<R> &b,const i
 	  h *= gamma;
 	  h -= Cg;  //  h = -Cg * gamma* h       
       }
-    if (nbitermax <= nbitermax  )
+    //    if (itermax <= nbitermax  )
 	cout << "CG does'nt converge: " << nbitermax <<   " ||g||^2 = " << g2 << " reps2= " << reps2 << endl; 
     return 0; 
 }
