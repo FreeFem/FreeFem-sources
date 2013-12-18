d205a0e119cee04ee79134a3aebd4440856feb41
Antoine Le Hyaric <Antoine Le Hyaric@localhost>
2013-12-18 16:18:08 +0100

    partial commit on DebianTesting64 (odyssee)

---
+++ src/lglib/lg.tab.hpp
@@ -0,0 +1,147 @@
+/* A Bison parser, made by GNU Bison 2.7.12-4996.  */
+
+/* Bison interface for Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_LG_LG_TAB_HPP_INCLUDED
+# define YY_LG_LG_TAB_HPP_INCLUDED
+/* Enabling traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 1
+#endif
+#if YYDEBUG
+extern int lgdebug;
+#endif
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     IF = 258,
+     ELSE = 259,
+     SET = 260,
+     GTGT = 261,
+     LTLT = 262,
+     OR = 263,
+     AND = 264,
+     NE = 265,
+     EQ = 266,
+     GE = 267,
+     LE = 268,
+     DOTSLASH = 269,
+     DOTSTAR = 270,
+     MOINSMOINS = 271,
+     PLUSPLUS = 272,
+     UNARY = 273,
+     LNUM = 274,
+     DNUM = 275,
+     CNUM = 276,
+     ID = 277,
+     FESPACEID = 278,
+     IDPARAM = 279,
+     STRING = 280,
+     ENDOFFILE = 281,
+     INCLUDE = 282,
+     LOAD = 283,
+     BIDON = 284,
+     FOR = 285,
+     WHILE = 286,
+     BREAK = 287,
+     CONTINUE = 288,
+     RETURN = 289,
+     TRY = 290,
+     CATCH = 291,
+     THROW = 292,
+     TYPE = 293,
+     FUNCTION = 294,
+     FESPACE = 295,
+     FESPACE1 = 296,
+     FESPACE3 = 297,
+     PLUSEQ = 298,
+     MOINSEQ = 299,
+     MULEQ = 300,
+     DIVEQ = 301,
+     DOTMULEQ = 302,
+     DOTDIVEQ = 303,
+     ARROW = 304,
+     BORDER = 305,
+     CURVE = 306,
+     SOLVE = 307
+   };
+#endif
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+{
+/* Line 2053 of yacc.c  */
+#line 126 "lg.ypp"
+ 
+ double dnum;
+ long lnum;
+ char * str;
+ char oper[8];
+ CC_F0 cexp;
+ Routine   *routine;
+ AC_F0 args;
+ aType type;
+ CListOfInst cinst;
+ Block * block; 
+ ListOfId *clist_id;
+/* ListCatch * clist_Catchs;*/
+
+
+/* Line 2053 of yacc.c  */
+#line 125 "lg.tab.hpp"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE lglval;
+
+#ifdef YYPARSE_PARAM
+#if defined __STDC__ || defined __cplusplus
+int lgparse (void *YYPARSE_PARAM);
+#else
+int lgparse ();
+#endif
+#else /* ! YYPARSE_PARAM */
+#if defined __STDC__ || defined __cplusplus
+int lgparse (void);
+#else
+int lgparse ();
+#endif
+#endif /* ! YYPARSE_PARAM */
+
+#endif /* !YY_LG_LG_TAB_HPP_INCLUDED  */
