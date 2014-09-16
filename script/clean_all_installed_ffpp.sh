9feb7dc5fe918938e45b606681012526a21e8ab6
Frederic Hecht <frederic.hecht@upmc.fr>
2014-09-16 15:15:30 +0200

    correct environment.cpp and Makefile.am to find and put plugin for mpi in correct dir.

--- script/clean_all_installed_ffpp.sh
+++ script/clean_all_installed_ffpp.sh
@@ -1,14 +1,14 @@
 #!/bin/sh 
 # clean all freefem++ version ...
 cd / 
-#if [ -d /Applications/FreeFem++.app ] ; then
-#  rm -rf /Applications/FreeFem++.app
-#fi
-if [ -d /usr/local/share/freefem++ ] ; then
-  rm -rf /usr/local/share/freefem++
+if [ -d Applications/FreeFem++.app ] ; then
+  rm -rf Applications/FreeFem++.app
 fi
-if [ -d  /usr/local/lib/ff++/ ]; then
- rm -rf /usr/local/lib/ff++
+if [ -d usr/local/share/freefem++ ] ; then
+  rm -rf usr/local/share/freefem++
+fi
+if [ -d  usr/local/lib/ff++/ ]; then
+ rm -rf usr/local/lib/ff++
 fi
 rm usr/local/bin/FreeFem++		usr/local/bin/FreeFem++-nw	usr/local/bin/ff-c++		usr/local/bin/ff-pkg-download 
 rm usr/local/bin/FreeFem++-CoCoa		usr/local/bin/bamg	usr/local/bin/ff-get-dep
