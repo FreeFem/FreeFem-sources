3932e8aa1ff7f38191b933618bd38079e40660fb
Frederic Hecht <frederic.hecht@upmc.fr>
2015-03-24 16:02:21 +0100

    add new schwarz interface

--- examples++-mpi/schwarz.cpp
+++ examples++-mpi/schwarz.cpp
@@ -1,6 +1,7 @@
-//ff-c++-LIBRARY-dep: cxx11   hpddm [petsc|mumps] umfpack amd  scalapack blas [mkl]   mpifc  fc mpi  pthread   
-//ff-c++-cpp-dep: 
-
+#ifndef _ALL_IN_ONE_
+#include <math.h>
+//ff-c++-LIBRARY-dep: cxx11   hpddm [petsc|mumps] umfpack amd  scalapack blas [mkl]   mpifc  fc mpi  pthread
+//ff-c++-cpp-dep:
 // mumps est avec petsc ..
 #ifdef WITH_mumps
 #define MUMPSSUB
@@ -16,7 +17,7 @@
 #endif
 
 
-#ifndef WITH_petsc 
+#ifndef WITH_petsc
 #pragma message("schwarz plugin compile without PETSc")
 #else
 #define MUMPSSUB
@@ -27,10 +28,15 @@
 #define HPDDM_FETI 0
 
 
-#include <math.h>
 #include <mpi.h>
 #include <ff++.hpp>
 #include "AFunction_ext.hpp"
+#ifdef BDD
+#undef BDD
+#endif
+#ifdef FETI
+#undef FETI
+#endif
 #include <HPDDM.hpp>
 
 template<class T>
@@ -60,8 +66,9 @@ class Pair {
         }
 };
 
-#ifdef WITH_petsc
+#ifdef WITH_PETSC
 #include "PETSc.hpp"
+#endif
 #endif
 
 namespace Schwarz {
@@ -132,7 +139,7 @@ class attachCoarseOperator_Op : public E_F0mps {
     public:
         Expression comm;
         Expression A;
-        static const int n_name_param = 7;
+        static const int n_name_param = 8;
         static basicAC_F0::name_and_type name_param[];
         Expression nargs[n_name_param];
         attachCoarseOperator_Op(const basicAC_F0& args, Expression param1, Expression param2) : comm(param1), A(param2) {
@@ -149,7 +156,8 @@ basicAC_F0::name_and_type attachCoarseOperator_Op<Type, K>::name_param[] = {
     {"threshold", &typeid(typename HPDDM::Wrapper<K>::ul_type)},
     {"parameters", &typeid(KN<long>*)},
     {"timing", &typeid(KN<double>*)},
-    {"ret", &typeid(Pair<K>*)}
+    {"ret", &typeid(Pair<K>*)},
+    {"solver", &typeid(long)}
 };
 template<class Type, class K>
 class attachCoarseOperator : public OneOperator {
@@ -201,26 +209,126 @@ AnyType attachCoarseOperator_Op<Type, K>::operator()(Stack stack) const {
     unsigned short nu = ptParm ? (*ptParm)[HPDDM::NU] : 20;
     nu = std::max(nu, static_cast<unsigned short>(1));
     std::pair<MPI_Request, const K*>* ret = nullptr;
-    if(mA) {
-        HPDDM::MatrixCSR<K> dA(mA->n, mA->m, mA->nbcoef, mA->a, mA->lg, mA->cl, mA->symetrique);
-        MatriceMorse<K>* mB = nargs[1] ? static_cast<MatriceMorse<K>*>(&(*GetAny<Matrice_Creuse<K>*>((*nargs[1])(stack))->A)) : nullptr;
-        MatriceMorse<K>* mP = nargs[2] ? static_cast<MatriceMorse<K>*>(&(*GetAny<Matrice_Creuse<K>*>((*nargs[2])(stack))->A)) : nullptr;
-        const HPDDM::MatrixCSR<K>* const dP = mP ? new HPDDM::MatrixCSR<K>(mP->n, mP->m, mP->nbcoef, mP->a, mP->lg, mP->cl, mP->symetrique) : nullptr;
-        double t = MPI_Wtime();
-        if(mB) {
-            HPDDM::MatrixCSR<K> dB(mB->n, mB->m, mB->nbcoef, mB->a, mB->lg, mB->cl, mB->symetrique);
-            ptA->template solveGEVP<EIGENSOLVER>(&dA, nu, threshold, &dB, dP);
+    double t;
+    if(mA || nargs[7]) {
+        long nbSolver = nargs[7] ? GetAny<long>((*nargs[7])(stack)) : 0;
+        std::vector<const HPDDM::MatrixCSR<K>*> vecAIJ;
+        if(mA) {
+            HPDDM::MatrixCSR<K> dA(mA->n, mA->m, mA->nbcoef, mA->a, mA->lg, mA->cl, mA->symetrique);
+            MatriceMorse<K>* mB = nargs[1] ? static_cast<MatriceMorse<K>*>(&(*GetAny<Matrice_Creuse<K>*>((*nargs[1])(stack))->A)) : nullptr;
+            MatriceMorse<K>* mP = nargs[2] ? static_cast<MatriceMorse<K>*>(&(*GetAny<Matrice_Creuse<K>*>((*nargs[2])(stack))->A)) : nullptr;
+            t = MPI_Wtime();
+            if(dA._n == dA._m) {
+                const HPDDM::MatrixCSR<K>* const dP = mP ? new HPDDM::MatrixCSR<K>(mP->n, mP->m, mP->nbcoef, mP->a, mP->lg, mP->cl, mP->symetrique) : nullptr;
+                if(mB) {
+                    HPDDM::MatrixCSR<K> dB(mB->n, mB->m, mB->nbcoef, mB->a, mB->lg, mB->cl, mB->symetrique);
+                    ptA->template solveGEVP<EIGENSOLVER>(&dA, nu, threshold, &dB, dP);
+                }
+                else
+                    ptA->template solveGEVP<EIGENSOLVER>(&dA, nu, threshold, nullptr, dP);
+                mA->nbcoef = dA._nnz;
+                mA->a = dA._a;
+                mA->lg = dA._ia;
+                mA->cl = dA._ja;
+                delete dP;
+            }
+            else {
+                vecAIJ.emplace_back(&dA);
+                nbSolver = 101;
+            }
         }
-        else
-            ptA->template solveGEVP<EIGENSOLVER>(&dA, nu, threshold, nullptr, dP);
-        mA->nbcoef = dA._nnz;
-        mA->a = dA._a;
-        mA->lg = dA._ia;
-        mA->cl = dA._ja;
+        else {
+            ptA->template interaction<false, true>(vecAIJ);
+            std::sort(vecAIJ.begin(), vecAIJ.end(), [](const HPDDM::MatrixCSR<K>* lhs, const HPDDM::MatrixCSR<K>* rhs) { return lhs->_m > rhs->_m; });
+            nbSolver = 100;
+        }
+        if(nbSolver != 0) {
+            ptA->callNumfact();
+            if(!vecAIJ.empty()) {
+                int dof = ptA->getDof();
+                HPDDM::Lapack<K> solver(dof);
+                const HPDDM::MatrixCSR<K>& first = *vecAIJ.front();
+                nu = std::min(nu, static_cast<unsigned short>(first._m));
+                K** ev = new K*[nu];
+                *ev = new K[nu * dof];
+                for(int i = 0; i < nu; ++i)
+                    ev[i] = *ev + i * dof;
+                ptA->setVectors(ev);
+                ptA->Type::super::initialize(nu);
+                int lwork = solver.workspace(&(first._m));
+                K* a;
+                typename HPDDM::Wrapper<K>::ul_type* values;
+                if(!std::is_same<K, typename HPDDM::Wrapper<K>::ul_type>::value) {
+                    a = new K[first._m * (2 * dof + first._m) + lwork];
+                    values = new typename HPDDM::Wrapper<K>::ul_type[nu + first._m + std::max(1, first._m * std::max(5 * first._m + 7, 2 * dof + 2 * first._m + 1))];
+                }
+                else {
+                    a = new K[first._m * (2 * dof + first._m + 1) + lwork + nu];
+                    values = reinterpret_cast<typename HPDDM::Wrapper<K>::ul_type*>(a + first._m * (2 * dof + first._m) + lwork);
+                }
+                int* pos = new int[nu + 8 * first._m];
+                std::fill(pos, pos + nu, 0);
+                std::fill(values, values + nu, 0.0);
+                for(const HPDDM::MatrixCSR<K>* A : vecAIJ) {
+                    K* u = a + dof * A->_m;
+                    K* vt = u + dof * A->_m;
+                    K* work = vt + A->_m * A->_m;
+                    typename HPDDM::Wrapper<K>::ul_type* s = values + nu;
+                    typename HPDDM::Wrapper<K>::ul_type* rwork = s + A->_m;
+                    std::fill(a, a + A->_m * dof, K(0.0));
+                    for(int i = 0; i < dof; ++i)
+                        for(int j = A->_ia[i]; j < A->_ia[i + 1]; ++j)
+                            a[i + A->_ja[j] * dof] = A->_a[j];
+                    ptA->Type::super::callSolve(a, A->_m);
+                    solver.svd(&(A->_m), a, s, u, vt, work, &lwork, pos + nu, rwork);
+                    for(unsigned int i = 0, j = 0, k = 0; k < nu; ++k) {
+                        if(s[i] > values[j])
+                            pos[k] = ++i;
+                        else
+                            pos[k] = -(++j);
+                    }
+                    for(unsigned int i = nu - 1; i > 0; ) {
+                        if(pos[i] < 0) {
+                            unsigned int j = i;
+                            while(j > 0 && pos[j - 1] < 0)
+                                --j;
+                            std::copy_backward(values - pos[j] - 1, values - pos[i], values + i + 1);
+                            std::copy_backward(ev[-pos[j] - 1], ev[-pos[i] - 1] + dof, ev[i] + dof);
+                            i = std::max(j, 1u) - 1;
+                        }
+                        else
+                            --i;
+                    }
+                    for(unsigned int i = 0; i < nu; ) {
+                        if(pos[i] > 0) {
+                            unsigned int j = i;
+                            while(j < nu - 1 && pos[j + 1] > 0)
+                                ++j;
+                            std::copy(s + pos[i] - 1, s + pos[j], values + i);
+                            std::copy(u + (pos[i] - 1) * dof, u + pos[j] * dof, ev[i]);
+                            i = j + 1;
+                        }
+                        else
+                            ++i;
+                    }
+                }
+                delete [] pos;
+                if(!std::is_same<K, typename HPDDM::Wrapper<K>::ul_type>::value)
+                    delete [] values;
+                delete [] a;
+                ptA->Type::super::initialize(nu);
+                if(nbSolver == 100)
+                    std::for_each(vecAIJ.begin(), vecAIJ.end(), std::default_delete<const HPDDM::MatrixCSR<K>>());
+            }
+            else
+                ptA->Type::super::initialize(0);
+        }
         if(timing)
             (*timing)[3] = MPI_Wtime() - t;
-        delete dP;
         parm[HPDDM::NU]           = nu;
+        MPI_Barrier(MPI_COMM_WORLD);
+        if(timing)
+            t = MPI_Wtime();
         if(ptA->exclusion(comm)) {
             if(pair)
                 pair->p = ptA->template buildTwo<1>(comm, parm);
@@ -233,14 +341,18 @@ AnyType attachCoarseOperator_Op<Type, K>::operator()(Stack stack) const {
             else
                 ret = ptA->template buildTwo<0>(comm, parm);
         }
+        if(timing)
+            (*timing)[4] = MPI_Wtime() - t;
     }
     else {
+        MPI_Barrier(MPI_COMM_WORLD);
         if(!threshold) {
             parm[HPDDM::NU]       = nu;
             ret = ptA->template buildTwo<2>(comm, parm);
         }
     }
-    delete ret;
+    if(ret)
+        delete ret;
     return 0L;
 }
 
@@ -298,13 +410,13 @@ AnyType solveDDM_Op<Type, K>::operator()(Stack stack) const {
     MatriceMorse<K>* mA = nargs[8] ? static_cast<MatriceMorse<K>*>(&(*GetAny<Matrice_Creuse<K>*>((*nargs[8])(stack))->A)) : 0;
     long solver = nargs[4] ? GetAny<long>((*nargs[4])(stack)) : 0;
     double timer = MPI_Wtime();
-    if(mA && (solver == 6 || solver == 7)) {
+    ptA->setType(solver == 1 || solver == 6 || solver == 11);
+    if(mpisize > 1 && (mA && (solver == 6 || solver == 8))) {
         HPDDM::MatrixCSR<K> dA(mA->n, mA->m, mA->nbcoef, mA->a, mA->lg, mA->cl, mA->symetrique);
         ptA->callNumfact(&dA);
     }
-    else
+    else if((solver != 100 && solver != 101) || ((solver == 100 || solver == 101) && mpisize == 1))
         ptA->callNumfact(nullptr);
-    ptA->setType(solver == 1 || solver == 6 || solver == 11);
     if(timing) (*timing)[1] = MPI_Wtime() - timer;
     long pipelined = nargs[5] ? GetAny<long>((*nargs[5])(stack)) : 0;
     bool excluded = nargs[6] ? GetAny<bool>((*nargs[6])(stack)) : false;
@@ -314,10 +426,14 @@ AnyType solveDDM_Op<Type, K>::operator()(Stack stack) const {
                 timer = MPI_Wtime();
             MPI_Wait(&(pair->p->first), MPI_STATUS_IGNORE);
             if(timing)
-                (*timing)[4] = MPI_Wtime() - timer;
+                (*timing)[timing->n - 1] = MPI_Wtime() - timer;
             delete [] pair->p->second;
             delete pair->p;
+            timer = MPI_Wtime();
         }
+    MPI_Barrier(MPI_COMM_WORLD);
+    if(!excluded && pair && pair->p && timing && mpisize > 1)
+        (*timing)[timing->n - 1] += MPI_Wtime() - timer;
     timer = MPI_Wtime();
     int rank;
     MPI_Comm_rank(ptA->getCommunicator(), &rank);
@@ -361,12 +477,41 @@ AnyType solveDDM_Op<Type, K>::operator()(Stack stack) const {
     return 0L;
 }
 
-double distributedDot(KN<double>* const& A, KN<double>* const& in, KN<double>* const& out) {
-    double* tmp = new double[in->n];
-    HPDDM::Wrapper<double>::diagv(in->n, *A, *in, tmp);
-    KN_<double> KN(tmp, in->n);
-    double dot = (KN, *out);
-    MPI_Allreduce(MPI_IN_PLACE, &dot, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+class distributedDot_Op : public E_F0mps {
+    public:
+        Expression A;
+        Expression in;
+        Expression out;
+        static const int n_name_param = 1;
+        static basicAC_F0::name_and_type name_param[];
+        Expression nargs[n_name_param];
+        distributedDot_Op(const basicAC_F0& args, Expression param1, Expression param2, Expression param3) : A(param1), in(param2), out(param3) {
+            args.SetNameParam(n_name_param, name_param, nargs);
+        }
+
+        AnyType operator()(Stack stack) const;
+};
+basicAC_F0::name_and_type distributedDot_Op::name_param[] = {
+    {"communicator", &typeid(pcommworld)}
+};
+class distributedDot : public OneOperator {
+    public:
+        distributedDot() : OneOperator(atype<double>(), atype<KN<double>*>(), atype<KN<double>*>(), atype<KN<double>*>()) { }
+
+        E_F0* code(const basicAC_F0& args) const {
+            return new distributedDot_Op(args, t[0]->CastTo(args[0]), t[1]->CastTo(args[1]), t[2]->CastTo(args[2]));
+        }
+};
+AnyType distributedDot_Op::operator()(Stack stack) const {
+    KN<double>* pA = GetAny<KN<double>*>((*A)(stack));
+    KN<double>* pin = GetAny<KN<double>*>((*in)(stack));
+    KN<double>* pout = GetAny<KN<double>*>((*out)(stack));
+    MPI_Comm* comm = nargs[0] ? (MPI_Comm*)GetAny<pcommworld>((*nargs[0])(stack)) : 0;
+    double* tmp = new double[pin->n];
+    HPDDM::Wrapper<double>::diagv(pin->n, *pA, *pin, tmp);
+    KN_<double> KN(tmp, pin->n);
+    double dot = (KN, *pout);
+    MPI_Allreduce(MPI_IN_PLACE, &dot, 1, MPI_DOUBLE, MPI_SUM, comm ? *((MPI_Comm*)comm) : MPI_COMM_WORLD);
     delete [] tmp;
     return dot;
 }
@@ -396,6 +541,20 @@ KN<K>* GlobalMV(KN<K>* Ax, GMV<Type*, KN<K>*> A) {
     return Ax;
 }
 
+#ifdef DEBUG_SCHWARZ
+template<class Type>
+long InitDDM(Type* const& A) {
+    A->setType(true);
+    A->callNumfact();
+    return 0;
+}
+template<class Type, class K>
+long ApplyAS(Type* const& A, KN<K>* const& in, KN<K>* const& out) {
+    A->apply(*in, *out);
+    return 0;
+}
+#endif
+
 template<template<class, char> class Type, class K, char S>
 void add() {
     Dcl_Type<Type<K, S>*>(Initialize<Type<K, S>>, Delete<Type<K, S>>);
@@ -407,48 +566,18 @@ void add() {
     TheOperators->Add("*", new OneOperator2<GMV<Type<K, S>*, KN<K>*>, Type<K, S>*, KN<K>*>(Build));
     TheOperators->Add("=", new OneOperator2<KN<K>*, KN<K>*, GMV<Type<K, S>*, KN<K>*>>(GlobalMV));
     if(std::is_same<K, double>::value)
-        Global.Add("dscalprod", "(", new OneOperator3_<double, KN<double>*, KN<double>*, KN<double>*>(distributedDot));
-}
-}
-
-
-
-static void  Load_Init() {
-    const char ds = 'S';
-    const char zs = 'G';
-    Schwarz::add<HpSchwarz, double, ds>();
-    zzzfff->Add("dschwarz", atype<HpSchwarz<double, ds>*>());
-    Schwarz::add<HpSchwarz, std::complex<double>, zs>();
-    zzzfff->Add("zschwarz", atype<HpSchwarz<std::complex<double>, zs>*>());
-#ifdef WITH_petsc
-    int argc = pkarg->n;
-    char** argv = new char*[argc];
-    for(int i = 0; i < argc; ++i)
-        argv[i] = const_cast<char*>((*(*pkarg)[i].getap())->c_str());
-    PetscInitialize(&argc, &argv, 0, "");
-    delete [] argv;
-    ff_atend(finalizePETSc);
-    Dcl_Type<DistributedCSR*>(Initialize<DistributedCSR>, Delete<DistributedCSR>);
-    Dcl_Type<GMV<DistributedCSR*, KN<double>*>>();
-    zzzfff->Add("dmatrix", atype<DistributedCSR*>());
-    TheOperators->Add("<-", new OneOperator1_<long, DistributedCSR*>(initEmptyCSR));
-    TheOperators->Add("<-", new initCSR<double>);
-    TheOperators->Add("*", new OneOperator2<GMV<DistributedCSR*, KN<double>*>, DistributedCSR*, KN<double>*>(Build));
-    TheOperators->Add("=", new OneOperator2<KN<double>*, KN<double>*, GMV<DistributedCSR*, KN<double>*>>(GlobalMV));
-    Global.Add("set", "(", new setOptions<double>());
-    Global.Add("distributedNumbering", "(", new OneOperator2_<long, DistributedCSR*, KN<double>*>(distributedNumbering));
-    Global.Add("renumber", "(", new OneOperator2_<long, DistributedCSR*, FEbaseArrayKn<double>*>(renumberCSR));
-    Global.Add("solvePETSc", "(", new OneOperator2_<long, DistributedCSR*, KN<double>*>(solvePETSc));
-    Dcl_Type<DistributedCSR_inv>();
-    TheOperators->Add("^", new OneBinaryOperatorPETSc());
-    Dcl_Type<Inv<DistributedCSR_inv, KN<double>*>>();
-    TheOperators->Add("*", new OneOperator2<Inv<DistributedCSR_inv, KN<double>*>, DistributedCSR_inv, KN<double>*>(Build));
-    TheOperators->Add("=", new OneOperator2<KN<double>*, KN<double>*, Inv<DistributedCSR_inv, KN<double>*>>(InvPETSc));
+        Global.Add("dscalprod", "(", new distributedDot);
+#ifdef DEBUG_SCHWARZ
+    Global.Add("initPreconditioner", "(", new OneOperator1_<long, Type<K, S>*>(InitDDM));
+    Global.Add("precond", "(", new OneOperator3_<long, Type<K, S>*, KN<K>*, KN<K>*>(ApplyAS));
 #endif
-    Dcl_Type<Pair<double>*>(InitP<Pair<double>>, Destroy<Pair<double>>);
-    zzzfff->Add("dpair", atype<Pair<double>*>());
-    Dcl_Type<Pair<std::complex<double>>*>(InitP<Pair<std::complex<double>>>, Destroy<Pair<std::complex<double>>>);
-    zzzfff->Add("zpair", atype<Pair<std::complex<double>>*>());
+}
 }
 
-LOADFUNC(Load_Init);
+#ifndef _ALL_IN_ONE_
+static void Init_Schwarz() {
+#include "init.hpp"
+}
+
+LOADFUNC(Init_Schwarz)
+#endif
