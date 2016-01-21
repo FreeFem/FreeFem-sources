1053cc8ec73176f87b8cf6f4429006490f039ced
Frederic Hecht <frederic.hecht@upmc.fr>
2016-01-21 21:07:31 +0100

    correct bug in complex solve / probleme finite element add tools to compute cuvature of border add tools to find closepoint in set of point

--- examples++-load/ClosePoints.cpp
+++ examples++-load/ClosePoints.cpp
@@ -3,7 +3,7 @@
 //#include "HashTable.hpp"
 using namespace Fem2D;
 
-
+static bool debug = false;
 
 class R2close {
 public:
@@ -13,10 +13,10 @@ public:
     Point *P;
     const double EPSILON;
     double x0,y0,x1,y1, coef;// boundin box
-    R2close() :data(0), n(0), nx(1000000), P(new Point[nx]),EPSILON(1e-6)  {InitialiserListe();}
+    R2close() :data(0), n(0), nx(1000000), P(new Point[nx]),EPSILON(1e-6),offset(0)  {InitialiserListe();}
     R2close(double *dd,int mx,double eps=1e-6,int offsett=1) :
-      data(dd), n(0), nx(mx), P(new Point[nx]),EPSILON(eps),offset(offsett)
-        {InitialiserListe();}
+    data(dd), n(0), nx(mx), P(new Point[nx]),EPSILON(eps),offset(offsett)
+    {InitialiserListe();}
     Point  operator[](int i) const { return P[i];}
     
     int N,m;  // le nombre de code = ncase()%n
@@ -43,10 +43,10 @@ public:
         
         coef  = 1./max(x1-x0,y1-y1);
         if(verbosity>10)
-        cout << "     bounding box ClosePoints  [" << x0 << ", "<< y0 << "], [ " << x1 << " " << y1 << "] " << endl;
+            cout << "     bounding box ClosePoints  Pmin=[" << x0 << ", "<< y0 << "], Pmax=[ " << x1 << " " << y1 << "] " << endl;
         
         N=max(sqrt(nx),10.);
-        m=max(nx/10,10);
+        m=max(nx/10,100);
         next = new int[nx];
         head = new int[m];
         for(int i=0; i< m;++i)
@@ -58,32 +58,36 @@ public:
         assert(n<nx);
         P[n]=p;
         
-       int k=ncase(x,y)%m;
+        int k=ncase(x,y)%m;
         assert(k>=0);
         next[n]=head[k];
         head[k]=n;
+        if(debug)   cout << n << " <- " << k << " / " << x << " " << y << " / "<< offset << endl;
         return n++;
     }
+private:
     Point * Exist(double *p) const
     {
         double x = p[0], y=p[offset];
-
+        
         for(int i=0;i<n;++i)
             if(Equivalent(x,y,P[i]))
                 return P+i;
         return 0;
     }
-    
+public:
     bool Equivalent(double x0,double y0,const Point Q) const
     {
-        return  max(abs(x0-Q[0]),abs(y0-Q[offset])) < EPSILON;
+        return  ((x0-Q[0])*(x0-Q[0])+(y0-Q[offset])*(y0-Q[offset])) < EPSILON*EPSILON;
     }
     
     Point * Exist(double x, double y,int k) const
     {
         for(int i=head[k%m] ; i!=NotaPoint; i=next[i] )
+        {   //cout << i << " ";
             if(Equivalent(x,y,P[i]))
                 return P+i;
+        }
         return 0;
     }
     
@@ -94,16 +98,19 @@ public:
         else return AddSimple(p);
     }
     
-    int AddOpt(double *p)
+    Point * Find(double x,double y)
     {
-        double x = p[0], y=p[offset];
-        double eps=EPSILON/2;
+      if(debug)   cout << " Find " << x << " "<< y  << " " << EPSILON << " " << ncase(x,y) << ": " ;
+       // double x = p[0], y=p[offset];
+        double eps=EPSILON;
         Point *q=0;
         int kk[9],k=0,nc;
         for(int i=-1;i<2;++i)
             for(int j=-1;j<2;++j)
             {
                 nc = ncase(x+eps*i,y+eps*j);
+                //cout <<x+eps*i << " " << y+eps*j << " " << nc << " . ";
+                if(nc>=0)
                 for(int i=0;i<k;++i) // remove double cas e..
                     if(kk[i]==nc)
                     {
@@ -111,7 +118,6 @@ public:
                         break;
                     }
                 if(nc>=0) kk[k++]=nc;
-                
             }
         if(k>4) {
             cout << "   ClosePoints: Bug ??? : " << k << " : " ;
@@ -123,8 +129,22 @@ public:
         for(int i=0;i<k;++i)
         {
             q=Exist(x,y,kk[i]) ;
+           if(debug)  cout<< " " << kk[i]  ;
+
             if(q) break;
         }
+     if(debug)    cout << " q= "<< q << endl;
+        if( q)
+            return  q; // numero du point
+        else return 0;
+    }
+    Point * Find(double *p)
+    {
+        return Find(*p,*(p+offset));
+    }
+    int AddOpt(double *p)
+    {
+        Point *q=Find(p);
         if( q)
             return  q-P; // numero du point
         else
@@ -134,7 +154,7 @@ public:
     long ncase(double x,double y)
     {
         if(x <x0 || x >=x1 || y <y0 || y >=y1 ) return -1; // dehors
-        else return  long((x-x0)/EPSILON) +  long((y-y0)/EPSILON)*N;// indice de la case contenant (x,y).
+        else return  long((x-x0)/2/EPSILON) +  long((y-y0)/2/EPSILON)*N;// indice de la case contenant (x,y).
     }
     ~R2close()
     {
@@ -157,29 +177,30 @@ double dist2(int n,double *p,double *q)
 }
 long Hcode(int n,double eps,double *p,double *p0);
 
-KN<long>* CloseTo(Stack stack,double const & eps,KNM_<double> const &  P,KNM<double> * const &  q,bool tq)
+KN<long>* CloseTo(Stack stack,double const & eps,KNM_<double> const &  P,KNM<double> * const &  q,bool tq,bool inv=0)
 {
     
-  long n0=P.N();
-  long m0=P.M();
-  if(verbosity>2 )
-    cout << " -ClosePoints Size array;   n0 " << n0 << " m0 " << m0  << endl;
- // if(t) std::swap(n0,m0);
-  ffassert(n0==2);
- //   bool tq=t; // bofbof ...
- // KNM_<double>  Po =*p;
- // KNM_<double>  Pt =p->t();
-  //KNM_<double> P(t ? Pt: Po);
-  KNM<double> & Qo=*q;
-   double * p0=&(P(0,0));
-   int offset10 =( &(P(1,0)) - p0);
-   int offset01 =( &(P(0,1)) - p0);
+    long n0=P.N();
+    long m0=P.M();
+    if(verbosity>2 )
+        cout << " -ClosePoints Size array;   n0 " << n0 << " m0 " << m0  << endl;
+    // if(t) std::swap(n0,m0);
+    ffassert(n0==2);
+    //   bool tq=t; // bofbof ...
+    // KNM_<double>  Po =*p;
+    // KNM_<double>  Pt =p->t();
+    //KNM_<double> P(t ? Pt: Po);
+    KNM<double> & Qo=*q;
+    double * p0=&(P(0,0));
+    
+    int offset10 =( &(P(1,0)) - p0);
+    int offset01 =( &(P(0,1)) - p0);
     if(verbosity>10 )
     {
-    cout << "     offset of 0 1 :  "<< offset01  << endl;
-    cout << "     offset of 1 0 :  "<< offset10 << endl;
+        cout << "     offset of 0 1 :  "<< offset01  << endl;
+        cout << "     offset of 1 0 :  "<< offset10 << endl;
     }
-  MeshPoint &mp= *MeshPointStack(stack); // the struct to get x,y, normal , value
+    MeshPoint &mp= *MeshPointStack(stack); // the struct to get x,y, normal , value
     
     double x0= P(0,':').min();
     double y0= P(1,':').min();
@@ -189,33 +210,36 @@ KN<long>* CloseTo(Stack stack,double const & eps,KNM_<double> const &  P,KNM<dou
     // add cc
     double dd= max(x1-x0,y1-y0)*0.01;
     double data[]={x0-dd,y0-dd,x1+dd,y1+dd};
+    KN<long>* pr=0;
+    if(inv) pr = new KN<long>(m0);
     
-    
-  R2close S(data,m0,eps,offset10);
-  for (int i=0; i<m0;++i)
+    R2close S(data,m0,eps,offset01);
+    for (int i=0; i<m0;++i)
     {
         if(verbosity>19 )
-        cout << i << " :: " << P(0,i) << " " << P(1,i) << endl;
-        S.AddOpt(&P(0,i));
+            cout << i << " :: " << P(0,i) << " " << P(1,i) << endl;
+        int j= S.AddOpt(&P(0,i));
+        if(pr) (*pr)[i]=j;
     }
     
-    KN<long>* pr=new KN<long>(S.n);
+    if( pr==0)
+        pr=new KN<long>(S.n);
     if(q)
     {
-    if(tq)
-      Qo.resize(S.n,n0);
-    else
-     Qo.resize(n0,S.n);
-    KNM_<double> Q(tq ? Qo.t() : Qo);
-   for(int i=0;i<S.n;++i)
-   {
-       double * pi=S[i];
-       (*pr)[i]= (pi-p0)/offset01;
-       for(int j=0; j<n0;++j)
-           Q(j,i)=pi[j*offset10];
-   }
+        if(tq)
+            Qo.resize(S.n,n0);
+        else
+            Qo.resize(n0,S.n);
+        KNM_<double> Q(tq ? Qo.t() : Qo);
+        for(int i=0;i<S.n;++i)
+        {
+            double * pi=S[i];
+            if(!inv) (*pr)[i]= (pi-p0)/offset01;
+            for(int j=0; j<n0;++j)
+                Q(j,i)=pi[j*offset10];
+        }
     }
-    else
+    else if(!inv)
     {
         for(int i=0;i<S.n;++i)
         {
@@ -223,70 +247,79 @@ KN<long>* CloseTo(Stack stack,double const & eps,KNM_<double> const &  P,KNM<dou
             (*pr)[i]= (pi-p0)/offset01;
         }
     }
-    if(verbosity>2 ) cout << "  - ClosePoint: nb of common points " << m0-pr->N();
- //   double *p0=&(*p)(0,0);
+    if(verbosity>2 ) cout << "  - ClosePoint: nb of common points " << m0-S.n;
+    //   double *p0=&(*p)(0,0);
     
     
-  return Add2StackOfPtr2FreeRC(stack,pr);
+    return Add2StackOfPtr2FreeRC(stack,pr);
 }
 
+template<bool inv>
 KN<long>* CloseTo(Stack stack,double const & eps,KNM<double> * const &  p,KNM<double> * const &  q)
 {
     KNM_<double> P(*p);
-    return CloseTo(stack,eps,P,q,false);
+    return CloseTo(stack,eps,P,q,false,inv);
     
 }
+
+template<bool inv>
 KN<long>* CloseTo(Stack stack,double const & eps,Transpose<KNM<double>  *>  const &  p,KNM<double> * const &  q)
 {
     KNM_<double> P(p.t->t());
-    return CloseTo(stack,eps,P,q,false);
+    return CloseTo(stack,eps,P,q,false,inv);
     
 }
+
+template<bool inv>
 KN<long>* CloseTo(Stack stack,double const & eps,Transpose<KNM<double>  *>  const &  p,Transpose<KNM<double>  *>  const &  q)
 {
     KNM_<double> P(p.t->t());
-    return CloseTo(stack,eps,P,q,true);
+    return CloseTo(stack,eps,P,q,true,inv);
     
 }
 
+template<bool inv>
 KN<long>* CloseTo(Stack stack,double const & eps,KNM<double> * const &  p,Transpose<KNM<double>  *> const &  q)
 {
     KNM_<double> P(*p);
-    return CloseTo(stack,eps,P,q.t,true);
+    return CloseTo(stack,eps,P,q.t,true,inv);
     
 }
 
+template<bool inv>
 KN<long>* CloseTo(Stack stack,double const & eps,KNM<double> * const &  p)
 {
     KNM_<double> P(*p);
-    return CloseTo(stack,eps,P,0,false);
+    return CloseTo(stack,eps,P,0,false,inv);
     
 }
+
+template<bool inv>
 KN<long>* CloseTo(Stack stack,double const & eps,KNM_<double>  const &  p)
 {
     KNM_<double> P(p);
     if(verbosity>5)
-    cout << " CloseTo KNM_ "<< P.N() << " " << P.M() << endl;
-    return CloseTo(stack,eps,P,0,false);
+        cout << " CloseTo KNM_ "<< P.N() << " " << P.M() << endl;
+    return CloseTo(stack,eps,P,0,false,inv);
     
 }
 
-
+template<bool inv>
 KN<long>* CloseTo(Stack stack,double const & eps,Transpose<KNM<double>  *>  const &  p)
 {
-     KNM_<double> P(p.t->t());
-    return CloseTo(stack,eps,P,0,false);
+    KNM_<double> P(p.t->t());
+    return CloseTo(stack,eps,P,0,false,inv);
 }
 
-
+template<bool inv>
 KN<long>* CloseTo(Stack stack,double const & eps,pmesh  const &  pTh,KNM<double> * const &  pq)
 {
-  ffassert(pTh && pq);
- const  Mesh & Th=*pTh;
-  const KNM<double> Q=*pq;
-  int np=Q.N();
-  assert(Q.M()>=2);
-  KN<long>* pr=new KN<long>(np);
+    ffassert(pTh && pq);
+    const  Mesh & Th=*pTh;
+    const KNM<double> Q=*pq;
+    int np=Q.N();
+    assert(Q.M()>=2);
+    KN<long>* pr=new KN<long>(inv? Th.nv :np);
     KN<int> b(Th.nv);
     b=0;
     for (int i=0;i<Th.nv;i++)
@@ -298,19 +331,19 @@ KN<long>* CloseTo(Stack stack,double const & eps,pmesh  const &  pTh,KNM<double>
     }
     
     
-  *pr=-1L;
-  R2 Pmin,Pmax;
-  Th.BoundingBox(Pmin,Pmax);
+    *pr=-1L;
+    R2 Pmin,Pmax;
+    Th.BoundingBox(Pmin,Pmax);
     int nv=b.sum();
-    cout << " Th.nv " <<Th.nv << " " << nv << "/ " << Pmin << " "<< Pmax <<endl;
+    if(verbosity>9) cout << " Th.nv " <<Th.nv << " " << nv << "/ " << Pmin << " "<< Pmax <<endl;
     FQuadTree * quadtree = new FQuadTree(pTh,Pmin,Pmax,nv); //  put all the old vertices in the quadtree
     //  copy of the old vertices
     for (int i=0;i<Th.nv;i++)
     {
-       if(b[i])
-       {  cout << i << " " << Th.vertices[i] << endl;
-             quadtree->Add(Th.vertices[i]);
-       }
+        if(b[i])
+        {  cout << i << " " << Th.vertices[i] << endl;
+            quadtree->Add(Th.vertices[i]);
+        }
         
     }
     cout << " After quadterr" << endl;
@@ -318,32 +351,143 @@ KN<long>* CloseTo(Stack stack,double const & eps,pmesh  const &  pTh,KNM<double>
     {
         R2 P(Q(j,0),Q(j,1));
         
-      Vertex * pV=quadtree->ToClose(P,eps);
+        Vertex * pV=quadtree->ToClose(P,eps);
         if(pV)
         {
-          pV=quadtree->NearestVertex(P);
-          long k= Th(pV);
-            cout << j << " " << k << " "  << P << " " << pV <<endl;
-
-          (*pr)[j]=k;
+            pV=quadtree->NearestVertex(P);
+            long k= Th(pV);
+            // cout << j << " " << k << " "  << P << " " << pV <<endl;
+            if(inv) (*pr)[k]=j;
+            else (*pr)[j]=k;
+            
         }
     }
     delete quadtree;
     
-  return Add2StackOfPtr2FreeRC(stack,pr);
+    return Add2StackOfPtr2FreeRC(stack,pr);
+}
+bool InterAB_Disq(R2 A,R2 B,R2 C,double r)
+{
+    double ACB = det(A,C,B);
+    if (ACB <0) return false;
+    R2 AB(A,B);
+    R2 AC(A,C),CB(C,B);
+    double LAB= AB.norme();
+    double H = ACB/4/LAB;
+    if( H > r) return false;
+    double AC2 = AC.norme2(),r2=r*r;
+    if(AC2 < r2) return true;
+    double CB2 = CB.norme2();
+    if(CB2 < r2) return true;
+    double ABAC=(AB,AC),ABCB=(AB,CB);
+    return   ABAC >0 && ABCB >0;
 }
 
+void Add(KN<long>& I,int i)
+{
+    int n= I.N();
+    int m =-I[n-1];
+    if( m <=0) {
+        m=n;
+        n = m*2;
+        I.resize(n);
+        I[n-1]=-m-1;
+    }
+    else --m;
+   if(debug)   cout << " add "<< m <<" "<< i <<" "<< n << endl;
+    I[m]=i;
+    if(m<n-1) --I[n-1];
+}
+void Clean(KN<long> &I)
+{
+    int n= I.N();
+    int m =-I[n-1];
+    if( m >=0) I.resize(m-1);
+}
+long Voisinage( KNM_<double> const &  P ,KNM_<double> const &  Q, double const &  eps,KN<KN<long> >  * const & IJ)
+{
+    int np=P.N();
+    int nq=Q.N();
+    int mp=P.M();
+    int mq=Q.M();
 
-void init(){
-    Global.Add("ClosePoints","(",new OneOperator2s_<KN<long>*,double, Transpose<KNM<double>  *>  >(CloseTo));
-    Global.Add("ClosePoints","(",new OneOperator2s_<KN<long>*,double, KNM<double>   *  >(CloseTo));
-    Global.Add("ClosePoints","(",new OneOperator2s_<KN<long>*,double, KNM_<double>     >(CloseTo));
+    double *p= &P(0,0);
+    double *q= &Q(0,0);
+    int offset01=&P(0,1)-p;
+    int offset10=&P(1,0)-p;
+    double * q0=&(Q(0,0));
+    int qoffset01 =( &(Q(0,1)) - q0);
+    int qoffset10 =( &(Q(1,0)) - q0);
+    ffassert( mp ==2);
+    ffassert( mq ==2);
     
-   Global.Add("ClosePoints","(",new OneOperator3s_<KN<long>*,double, Transpose<KNM<double>  *> ,KNM<double> * >(CloseTo));
-    Global.Add("ClosePoints","(",new OneOperator3s_<KN<long>*,double, Transpose<KNM<double>  *> ,Transpose<KNM<double>  *> >(CloseTo));
-   Global.Add("ClosePoints","(",new OneOperator3s_<KN<long>*,double,KNM<double> * ,KNM<double> * >(CloseTo));
-    Global.Add("ClosePoints","(",new OneOperator3s_<KN<long>*,double,KNM<double> * ,Transpose<KNM<double>  *> >(CloseTo));
-  Global.Add("ClosePoints","(",new OneOperator3s_<KN<long>*,double,pmesh,KNM<double> * >(CloseTo));
+    
+    IJ->resize(nq);
+    for (int i=0;i<nq ;i++)
+    {
+        (*IJ)[i].resize(2);
+        (*IJ)[i][0]=-1;
+        (*IJ)[i][1]=-1;
+    }
+    if(verbosity>99)  cout << " offset01 " <<offset01 << " " << offset10 << " p" << p << " " << np << " "<< P.M()  <<  endl;
+    // store - the size in last value of IJ[i][j]
+    double data[4];
+    data[0] = data[2] = p[0];
+    data[1] = data[3] = p[offset01];
+    for(int i=0,k=0; i<np; ++i, k+= offset10)
+    {
+        data[0] = min(data[0], p[k]);
+        data[2] = max(data[2], p[k]);
+        data[1] = min(data[1], p[k+offset01]);
+        data[3] = max(data[3], p[k+offset01]);
+        
+    }
+    double eps2 = eps+eps;
+    data[0] -=  eps2;
+    data[2] +=  eps2;
+    data[1] -=  eps2;
+    data[3] +=  eps2;
+    
+    R2close SP(data,np,eps,offset01);
+    for (int i=0; i<np;++i)
+        SP.AddSimple(&P(i,0));
+    for (int j=0; j<nq;++j)
+    {
+        R2close::Point *pi=SP.Find(Q(j,0),Q(j,1));
+        if( pi)
+        {  //
+            int i = (pi-SP.P);
+            if(verbosity>99)  cout << " Add to i=" << i << " -> j " << j << endl;
+            Add((*IJ)[i],j);
+        }
+    }
+    for (int j=0; j<nq;++j)
+        Clean((*IJ)[j]);
+    return 0;
+}
+void init()
+{
+    Global.Add("Voisinage","(",new OneOperator4_<long, KNM_<double> , KNM_<double>  ,double,KN<KN<long> > *   >(Voisinage));
+    
+    //   numbering ..
+    Global.Add("ClosePoints","(",new OneOperator2s_<KN<long>*,double, Transpose<KNM<double>  *>  >(CloseTo<false>));
+    Global.Add("ClosePoints","(",new OneOperator2s_<KN<long>*,double, KNM<double>   *  >(CloseTo<false>));
+    Global.Add("ClosePoints","(",new OneOperator2s_<KN<long>*,double, KNM_<double>     >(CloseTo<false>));
+    Global.Add("ClosePoints","(",new OneOperator3s_<KN<long>*,double, Transpose<KNM<double>  *> ,KNM<double> * >(CloseTo<false>));
+    Global.Add("ClosePoints","(",new OneOperator3s_<KN<long>*,double, Transpose<KNM<double>  *> ,Transpose<KNM<double>  *> >(CloseTo<false>));
+    Global.Add("ClosePoints","(",new OneOperator3s_<KN<long>*,double,KNM<double> * ,KNM<double> * >(CloseTo<false>));
+    Global.Add("ClosePoints","(",new OneOperator3s_<KN<long>*,double,KNM<double> * ,Transpose<KNM<double>  *> >(CloseTo<false>));
+    Global.Add("ClosePoints","(",new OneOperator3s_<KN<long>*,double,pmesh,KNM<double> * >(CloseTo<false>));
+    // inv  numbering ..
+    Global.Add("ClosePoints1","(",new OneOperator2s_<KN<long>*,double, Transpose<KNM<double>  *>  >(CloseTo<true>));
+    Global.Add("ClosePoints1","(",new OneOperator2s_<KN<long>*,double, KNM<double>   *  >(CloseTo<true>));
+    Global.Add("ClosePoints1","(",new OneOperator2s_<KN<long>*,double, KNM_<double>     >(CloseTo<true>));
+    Global.Add("ClosePoints1","(",new OneOperator3s_<KN<long>*,double, Transpose<KNM<double>  *> ,KNM<double> * >(CloseTo<true>));
+    Global.Add("ClosePoints1","(",new OneOperator3s_<KN<long>*,double, Transpose<KNM<double>  *> ,Transpose<KNM<double>  *> >(CloseTo<true>));
+    Global.Add("ClosePoints1","(",new OneOperator3s_<KN<long>*,double,KNM<double> * ,KNM<double> * >(CloseTo<true>));
+    Global.Add("ClosePoints1","(",new OneOperator3s_<KN<long>*,double,KNM<double> * ,Transpose<KNM<double>  *> >(CloseTo<true>));
+    Global.Add("ClosePoints1","(",new OneOperator3s_<KN<long>*,double,pmesh,KNM<double> * >(CloseTo<true>));
+    
 }
 
 LOADFUNC(init);
--- src/fflib/array_resize.hpp
+++ src/fflib/array_resize.hpp
@@ -9,7 +9,8 @@ template<class T> T *resize1(const Resize<T> & t,const long &n)
  }
 
 template<class T> T *resizeandclean1(const Resize<T> & t,const long &n)
- {  
+ {
+     
   int nn= t.v->N(); // old size 
   
   for (int i=n;i<nn;i++)  {delete (*t.v)[i];} // clean
@@ -17,7 +18,8 @@ template<class T> T *resizeandclean1(const Resize<T> & t,const long &n)
   for (int i=nn;i<n;i++)  {(*t.v)[i]=0;}  
   return  t.v;
  }
- 
+
+
 template<class T> T *resize2(const Resize<T> & t,const long &n, const long & m)
  {  
   t.v->resize(n,m);
