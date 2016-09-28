f39960f5dc20bf76b4cd80565e76e6481f5717bd
Cedric Doucet <cedric.doucet@inria.fr>
2016-09-28 17:41:17 +0200

    creation of recipe file for scotch library

--- cmake/modules/InstallScotch.cmake
+++ cmake/modules/InstallScotch.cmake
@@ -1,16 +1,8 @@
 SET(LIBNAME SCOTCH)
-
-
-IF(EXISTS ${${LIBNAME}_INSTALL_DIR})
-    MESSAGE(STATUS "${LIBNAME} is declared to be installed in ${${LIBNAME}_INSTALL_DIR}")
-ELSE()
-    IF(ENABLE_DOWNLOAD)
-        SET(${LIBNAME}_URL https://gforge.inria.fr/frs/download.php/file/34618/scotch_6.0.4.tar.gz)
-        SET(${LIBNAME}_ARGS -D CMAKE_INSTALL_PREFIX=<INSTALL_DIR>)
-        SET(${LIBNAME}_INSTALL_COMMAND cd .)
-    ELSE(ENABLE_DOWNLOAD)
-        MESSAGE(FATAL_ERROR "${LIBNAME} not found. Please provide a value to ${LIBNAME}_DIR or set ENABLE_DOWNLOAD to True")
-    ENDIF()
-ENDIF()
+SET(URL https://gforge.inria.fr/frs/download.php/file/34618/scotch_6.0.4.tar.gz)
+SET(URL_MD5 d58b825eb95e1db77efe8c6ff42d329f)
+SET(CMAKE_ARGS -D CMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+               -D CMAKE_C_COMPILER=${CMAKE_C_COMPILER})
+#SET(INSTALL_COMMAND cd .)
 
 
