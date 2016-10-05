cb3ce9d8be80ba95cf2c40e3e62f34e23d7d43cd
Cedric Doucet <cedric.doucet@inria.fr>
2016-10-05 20:40:14 +0200

    Management of the example tetgen.cpp

--- examples++-load/ffrandom.cpp
+++ examples++-load/ffrandom.cpp
@@ -2,7 +2,6 @@
 // ---------------------------------------------------------------------                                                     
 // $Id$                                                              
 
-#include "config.h"
 #include  <iostream>
 #include  <cfloat>
 using namespace std;
