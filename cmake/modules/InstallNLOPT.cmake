e74ccc79e709cc62aa9feed19bd8a33e798cfff9
Cedric Doucet <cedric.doucet@inria.fr>
2016-11-08 14:15:23 +0100

    Recipe for NLOPT

--- cmake/modules/InstallNLOPT.cmake
+++ cmake/modules/InstallNLOPT.cmake
@@ -1,7 +1,9 @@
 SET(LIBNAME NLOPT)
 SET(URL http://ab-initio.mit.edu/nlopt/nlopt-2.2.4.tar.gz)
 SET(URL_MD5 9c60c6380a10c6d2a06895f0e8756d4f)
-
+SET(CONFIGURE_COMMAND ../src/configure --prefix=<INSTALL_DIR>)
+SET(BUILD_COMMAND make)
+SET(INSTALL_COMMAND make install)
 
 
 
