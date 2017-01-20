267beea0f41f1226e77a1aecd946d9e248314900
Cedric Doucet <cedric.doucet@inria.fr>
2017-01-20 13:55:42 +0100

    Automatic detection of METIS and automatic installation of GMM++

--- cmake/modules/InstallGMM++.cmake
+++ cmake/modules/InstallGMM++.cmake
@@ -1,5 +1,6 @@
 SET(LIBNAME GMM++)
-SET(URL http://download.gna.org/getfem/stable/gmm-4.2.tar.gz)
-SET(URL_MD5 35fe900f7459c49f4b4337dc691c8cdf)
+SET(URL http://download.gna.org/getfem/stable/gmm-5.1.tar.gz)
+SET(URL_MD5 b16a65c09ea090fcd37cc3f47b692284)
+SET(CONFIGURE_COMMAND ../src/configure --prefix=<INSTALL_DIR>)
 
 
