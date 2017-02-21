4d028a941224f8ce821b9b44aacdbbaee37b89af
Cedric Doucet <cedric.doucet@inria.fr>
2017-02-21 17:48:34 +0100

    Unified management of system-based and download-based packages

--- cmake/modules/InstallFFTW.cmake
+++ cmake/modules/InstallFFTW.cmake
@@ -1,6 +1,8 @@
 SET(LIBNAME FFTW)
+
 SET(URL http://www.fftw.org/fftw-3.3.2.tar.gz)
 SET(URL_MD5 6977ee770ed68c85698c7168ffa6e178)
+
 SET(CONFIGURE_COMMAND  <SOURCE_DIR>/configure 
                        --disable-dependency-tracking  
                        --disable-fortran 
@@ -9,5 +11,3 @@ SET(CONFIGURE_COMMAND  <SOURCE_DIR>/configure
                        CC=${CMAKE_C_COMPILER}  
                        CFLAGS=${CMAKE_C_FLAGS} 
                        CXXFLAGS=${CMAKE_CXX_FLAGS})
-
-
--- cmake/modules/InstallTETGEN.cmake
+++ cmake/modules/InstallTETGEN.cmake
@@ -1,7 +1,9 @@
 SET(LIBNAME TETGEN)
+
+SET(URL http://www.tetgen.org/1.5/src/tetgen1.5.1-beta1.tar.gz)
+SET(URL_MD5 3d55c197bcbfc611b7ced6f343643756)
+
 SET(CMAKE_ARGS -D CMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} 
                -D CMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS} 
                -D CMAKE_INSTALL_PREFIX=<INSTALL_DIR>)
-SET(INSTALL_COMMAND cp libtet.a ../install)
-SET(URL http://www.tetgen.org/1.5/src/tetgen1.5.1-beta1.tar.gz)
-SET(URL_MD5 3d55c197bcbfc611b7ced6f343643756)
+SET(INSTALL_COMMAND mkdir ../install/lib && cp libtet.a ../install/lib && mkdir ../install/include && cp <SOURCE_DIR>/tetgen.h ../install/include)
