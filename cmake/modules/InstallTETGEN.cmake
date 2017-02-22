0a2b71dc53582a6235351c9d80efcbbcd12a4108
Cedric Doucet <cedric.doucet@inria.fr>
2017-02-22 17:41:23 +0100

    Add -p to mkdir for tetgen installation

--- cmake/modules/InstallTETGEN.cmake
+++ cmake/modules/InstallTETGEN.cmake
@@ -6,6 +6,6 @@ SET(URL_MD5 3d55c197bcbfc611b7ced6f343643756)
 SET(CMAKE_ARGS -D CMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} 
                -D CMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS} 
                -D CMAKE_INSTALL_PREFIX=<INSTALL_DIR>)
-SET(INSTALL_COMMAND mkdir ../install/lib && cp libtet.a ../install/lib && mkdir ../install/include && cp <SOURCE_DIR>/tetgen.h ../install/include)
+SET(INSTALL_COMMAND mkdir -p ../install/lib && cp libtet.a ../install/lib && mkdir -p ../install/include && cp <SOURCE_DIR>/tetgen.h ../install/include)
 
 SET(LIBRARIES libtet.a)
