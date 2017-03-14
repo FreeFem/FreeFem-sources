2d492104a7f1cc450dbebcf08d3c1b0c44c5ae7e
Cedric Doucet <cedric.doucet@inria.fr>
2017-03-14 17:51:13 +0100

    Fix a problem in installation of tetgen

--- cmake/modules/InstallTETGEN.cmake
+++ cmake/modules/InstallTETGEN.cmake
@@ -6,6 +6,6 @@ SET(URL_MD5 3d55c197bcbfc611b7ced6f343643756)
 SET(CMAKE_ARGS -D CMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} 
                -D CMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS} 
                -D CMAKE_INSTALL_PREFIX=<INSTALL_DIR>)
-SET(INSTALL_COMMAND mkdir -p ../install/lib && cp libtet.a ../install/lib && mkdir -p ../install/include && cp <SOURCE_DIR>/tetgen.h ../install/include)
+SET(INSTALL_COMMAND mkdir -p <INSTALL_DIR>/lib && cp libtet.a <INSTALL_DIR>/lib && mkdir -p <INSTALL_DIR>/include && cp <SOURCE_DIR>/tetgen.h <INSTALL_DIR>/include)
 
 SET(LIBRARIES libtet.a)
