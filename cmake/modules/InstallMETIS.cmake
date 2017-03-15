4faf5bc3ff6707d0a8a5be7cf5a3fb2888c29b88
Cedric Doucet <cedric.doucet@inria.fr>
2017-03-15 12:58:55 +0100

    Generate shared libraries instead of static ones for superlu and metis

--- cmake/modules/InstallMETIS.cmake
+++ cmake/modules/InstallMETIS.cmake
@@ -6,6 +6,8 @@ SET(URL_MD5 5465e67079419a69e0116de24fce58fe)
 SET(CMAKE_ARGS -D CMAKE_C_COMPILER=${CMAKE_C_COMPILER}
                -D CMAKE_C_FLAGS=${CMAKE_C_FLAGS}
                -D CMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+               -D SHARED=true
                -D GKLIB_PATH=../src/GKlib)
 
 SET(LIBRARIES libmetis.a)
+
