5a0fd1838288a01033730e7fccdb627bbb3da01d
Cedric Doucet <cedric.doucet@inria.fr>
2017-03-16 21:36:01 +0100

    patch command for suitesparse (copy of cmake script)

--- cmake/modules/InstallSUITESPARSE.cmake
+++ cmake/modules/InstallSUITESPARSE.cmake
@@ -2,4 +2,4 @@ SET(LIBNAME SUITESPARSE)
 SET(URL http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-4.4.4.tar.gz)
 SET(URL_MD5 e0af74476935c9ff6d971df8bb6b82fc)
 
-
+SET(PATCH_COMMAND cp ${CMAKE_SOURCE_DIR}/cmake/modules/suitesparse/CMakeLists.txt <SOURCE_DIR>)
