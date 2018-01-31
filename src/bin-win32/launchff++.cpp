e8e837dd50bc392a40856fbd0639581c3187dcb1
Frederic Hecht <frederic.hecht@upmc.fr>
2018-01-31 11:24:18 +0100

    reput old  launchff++.cpp and add dc Edge element of finite volume

--- src/bin-win32/launchff++.cpp
+++ src/bin-win32/launchff++.cpp
@@ -4,77 +4,116 @@
 #include <string>
 #include <cstring>
 #include <iostream>
+#include <tchar.h>
+#include <mmc.h>
 using namespace std;
 
 
-const char C='"';
+const string  CB="\"",CE="\"";
 
 
 const char SLACH='/';
 const char BACKSLACH='\\';
-#ifdef PURE_WIN32
 const  char dirsep=BACKSLACH, dirnsep=SLACH;
-#else
-const  char dirnsep=BACKSLACH, dirsep=SLACH;
-#endif
-string DirName(const char * f)
+string escapepath(string f)
 {
-    const char *c= strrchr(f,dirsep);
-    if(!c) return string("");
-    else return string(f,strlen(f)-strlen(c))+dirsep;
+	 string r;
+		for(int i=0; i< f.length(); ++i)
+		{
+		  if(f[i]==' '||f[i]=='('||f[i]==')'||f[i]=='=' )
+		   r.append("^");	  
+          r.append(f.begin()+i,f.begin()+i+1)	;		  
+			
+		}
+	return r; 
+}
+string MyGetModuleFileName()
+{
+  //  const size_t lx=2048;
+  //  char b[lx+1];
+    char fullPath[1024];
+    string r,f;
+    int lp=0;
+    int l=GetModuleFileName(NULL, fullPath, 1024);
+    if( l >0)
+    {
+	
+	   f=fullPath;
+	   r = escapepath(f); 
+      
+    }
+    //cout << l <<" MyGetModuleFileName " << fullPath << "\n" 
+	//     << r << "\n" << f << "*****"<< endl;
+       return r;
+}
+string DirName(const string & fn)
+{
+    //cout << " dir ff++ " << fn<< " ::" << dirsep << endl;
+
+    size_t s=fn.rfind(dirsep);
+    
+    if( s == string::npos ) return string();
+    //cout<< " **" << string(fn,0,s) << " s " << s << endl;
+    return string(fn,0,s)+dirsep;
 }
 int main(int argc,const char **argv)
 {
-  int debug=0; 
-  char *dirff=0;
-  const char *pp=0;
-  LPWSTR buffer; //or wchar_t * buffer;
-  int lbuffer = GetModuleFileName(NULL, buffer, MAX_PATH) ;
-  string dirff;
-  if lbuffer>0 dirff = DirName(buffer);
+	string SC=" ";
+	string addargs=SC+"-wait"+SC+"-log";
+
+    int debug=0;
+    string pp;
     
-  string cmd=dirff+"freefem++.exe ";
-  if(argc <=1)
-  {
+    string cmd;
+
+
+    cmd +=DirName(MyGetModuleFileName()); 
+	cmd += "FreeFem++.exe" ;
+ 
+
+	
+    if(debug) cout << "cmd:: " << cmd << endl;
+    if(argc <=1)
+    {
         cerr << " Sorry no file name "<< endl;
-        cerr << " Drag and Drop the file icon on the application  icon or double clip on script file" << endl;
-      cmd += " -wait -log";
-      int ret= system(cmd.c_str());
-      return 0; 
-  }
-  
-  for(int i=1;i<argc;++i)
-    {	
-	if(strcmp("++d",argv[i])==0) 
-	  debug=1;
-	else {
-    cmd += C;
-    cmd += argv[i];
-    if(!pp &&strlen(argv[i])>2) 
-	if( argv[i][1]==':')
-          pp= argv[i];
-    cmd += C;
-    cmd += " ";
-	if( debug) cout << "  ffl: arg " << i << argv[i] << endl;
-    }}
-  if(pp)
-   {
-   	if( debug ) cout << "  ffl: file:" << pp << endl;  
-    int i=0;
-    int l= strlen(pp);
-     for(i=l-1;i>=0;i--)
-       if(pp[i]=='\\') break;
-     dir= new char [l+1];
-     strcpy(dir,pp);
-     dir[i]=0;
-	 if(debug) 
-     cout << "  ffl:  chdir to " << dir << endl;
-     _chdir(dir);
-     delete [] dir;
-   }
-   cmd += " -wait -log";
-   if(debug) 
-   cout << "exec " << cmd << endl;
-   int ret= system(cmd.c_str());
-   return ret;
+        cerr << " Drag and Drop the file icon on the application  icon or double clip on script file"
+             << endl;
+        cmd += addargs;
+        int ret= system(cmd.c_str());
+        return 0;
+    }
+    
+    for(int i=1;i<argc;++i)
+    {
+        if(strcmp("++d",argv[i])==0)
+            debug=1;
+        else {
+			cmd +=SC;
+           	cmd += CB;		
+            cmd += argv[i];
+			cmd += CE;
+            if(!pp.length() && strlen(argv[i])>2)
+                if( argv[i][1]==':')
+                    pp= argv[i];
+			;
+            if( debug) cout << "  ffl: arg " << i << argv[i] << endl;
+        }}
+    if(pp.length() )
+    {
+        if( debug ) cout << "  ffl: file:" << pp << endl;
+        string dir = DirName(pp);
+        if(debug)
+            cout << "  ffl:  chdir to " << dir << endl;
+        _chdir(dir.c_str());
+
+    }
+	cout << flush;
+   cerr << "\n*" << cmd <<"*\n";
+
+    cmd += addargs;
+    cerr << "\n**" << cmd <<"**\n";
+    if(debug) 
+        cout << "exec " << cmd << endl;
+    int ret= system(cmd.c_str());
+    return ret;
 }
