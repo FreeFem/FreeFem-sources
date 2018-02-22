6a9899d12ec2c489c33dcf52c89f5ca3124eb0d3
Frederic Hecht <frederic.hecht@upmc.fr>
2018-02-22 14:20:55 +0100

    add missing file in distrib + compil ffmester

--- examples++-load/libff-mmap-semaphore.h
+++ examples++-load/libff-mmap-semaphore.h
@@ -1,4 +1,3 @@
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -27,22 +26,38 @@ struct FF_P_mmap {
 typedef  struct FF_P_mmap *ff_Pmmap ;
 typedef  struct FF_P_sem *ff_Psem ;
 ff_Psem ffsem_malloc();
+
 void ffsem_del(ff_Psem p);
+void ffsem_del_(long * p);
 void ffsem_destroy(ff_Psem p);
+void ffsem_destroy_(long * p);
 void ffsem_init0(ff_Psem p);
+void ffsem_init0_(long * p);
 void ffsem_init(ff_Psem p,const char *  nmm,int crea);
+void ffsem_init_(long * p, const char *nm,int * crea,int lennm);
 long ffsem_post(ff_Psem p);
+void ffsem_post_(long * p, long * ret);
 long ffsem_wait(ff_Psem p);
+void ffsem_wait_(long * p, long * ret);
 long ffsem_trywait(ff_Psem p);
+void ffsem_trywait_(long * p, long * ret);
 
 ff_Pmmap ffmmap_malloc();
+
 void ffmmap_del(ff_Pmmap p);
+void ffmmap_del_(long * p);
 void ffmmap_destroy(ff_Pmmap p);
+void ffmmap_destroy_(long * p);
 void ffmmap_init0(ff_Pmmap p);
+void ffmmap_init0_(long * p);
 long ffmmap_msync(ff_Pmmap p,long off, long ln);
+void ffmmap_msync_(long * p,int * off,int * ln, long * ret);
 void ffmmap_init(ff_Pmmap p,const char *  nmm,long len);
-long ffmmap_read(ff_Pmmap p,void *t,size_t n,size_t off);
-long ffmmap_write(ff_Pmmap p,void *t,size_t n,size_t off);
+void ffmmap_init_(long * p,const  char *nm,int * len,int lennm);
+long ffmmap_read(ff_Pmmap p,void *t,size_t n,long off);
+void ffmmap_read_(long * p,void *pt,int *ln,int *off, long * ret);
+long ffmmap_write(ff_Pmmap p,void *t,size_t n,long off);
+void ffmmap_write_(long * p,void *pt,int *ln,int * off, long * ret);
 
 #ifdef __cplusplus
 }
