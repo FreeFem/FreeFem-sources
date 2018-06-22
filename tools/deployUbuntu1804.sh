67cf734adeb6991bae8b4bf10a95b9f9e170d158
Simon Garnotel <simon.garnotel@gmail.com>
2018-06-22 11:27:33 +0200

    modify deployment scripts

--- tools/deployUbuntu1804.sh
+++ tools/deployUbuntu1804.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Lesser General Public License #
 # along with FreeFem++. If not, see <http://www.gnu.org/licenses/>.        #
 ############################################################################
-# SUMMARY : Deploy FreeFem++ binary on Ubuntu 16.04
+# SUMMARY : Deploy FreeFem++ binary on Ubuntu 18.04
 # LICENSE : LGPLv3
 # ORG     : LJLL Universite Pierre et Marie Curie, Paris, FRANCE
 # AUTHORS : Simon Garnotel
@@ -24,7 +24,13 @@
 #           Frederic Hecht
 # E-MAIL  : simon.garnotel@gmail.com
 
+# For Ubuntu 18.04 up to date (21/06/2018)
+
+## WARNING: this script uses binary compiled on Ubuntu 16.04
+##          some libraries are NOT compatible
+
 ## TODO: hdf5 does not work properly
+##       incompatible gfortran
 
 # Colors
 bold="\e[1m"
@@ -34,15 +40,27 @@ yellowBold="\e[1m\e[33m"
 reset="\e[0m"
 
 # Error functions
+errLink=0
 errCopy=0
 errConfig=0
 errBashrc=0
+checkErrorLink () {
+	if [ $? -eq 0 ]
+	then
+		echo -e "$greenBold INFO:$reset Linkage success"
+	else
+		echo -e "$yellowBold WARNING:$reset Linkage failed"
+		echo -e "\tThe file probably already exists. This is not critical"
+		((errLink++))
+	fi
+}
+
 checkErrorCopy () {
 	if [ $? -eq 0 ]
 	then
 		echo -e "$greenBold INFO:$reset Copy success"
 	else
-		echo -e "CRITICAL:$reset Copy failed"
+		echo -e "$redBold CRITICAL:$reset Copy failed"
 		((errCopy++))
 	fi
 }
@@ -68,6 +86,19 @@ checkErrorBashrc () {
 		((errBashrc++))
 	fi
 }
+
+# Need to relink some libraries
+echo -e "$bold Linkage$reset"
+echo -e "\tRelink lbhdf5"
+ln -s /usr/lib/x86_64-linux-gnu/libhdf5_serial.so.20 /usr/lib/x86_64-linux-gnu/libhdf5_serial.so.10
+checkErrorLink
+
+echo -e "\tRelink libmpi"
+ln -s /usr/lib/x86_64-linux-gnu/libmpi_cxx.so /usr/lib/x86_64-linux-gnu/libmpi_cxx.so.1
+checkErrorLink
+ln -s /usr/lib/x86_64-linux-gnu/openmpi/lib/libmpi.so /usr/lib/x86_64-linux-gnu/libmpi.so.12
+checkErrorLink
+
 
 # Copy binaries to /usr/local/*
 echo -e "$bold Deploy$reset"
@@ -103,10 +134,15 @@ fi
 
 # Summary
 echo -e "$bold Summary$reset"
+echo -e "\tLink error: $errLink"
 echo -e "\tCopy error: $errCopy"
 echo -e "\tFile error: $errConfig"
 echo -e "\tBashrc error: $errBashrc"
 
+if [ $errLink -gt 0 ]
+then
+	echo -e "$yellowBold Some link errors occurs$reset"
+fi
 if [ $errCopy -gt 0 ]
 then
 	echo -e "$redBold Some copy errors occurs$reset"
@@ -120,7 +156,7 @@ then
 	echo -e "$redBold Some bashrc errors occurs$reset"
 fi
 
-if [ $errCopy -eq 0 ] && [ $errConfig -eq 0 ] && [ $errBashrc -eq 0 ]
+if [ $errLink -eq 0 ] && [ $errCopy -eq 0 ] && [ $errConfig -eq 0 ] && [ $errBashrc -eq 0 ]
 then
 	echo -e "$greenBold You can now use the FreeFem++ command directly in your terminal$reset"
 	echo -e " Complete integration needs a restart"
