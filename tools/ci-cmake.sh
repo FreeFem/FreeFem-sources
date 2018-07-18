9cfe7e48ed1e4e835988c818680408608e200b21
Cedric Doucet <cedric.doucet@inria.fr>
2018-07-18 14:33:28 +0200

    Export LD_LIBRARY_PATH in CI

--- tools/ci-cmake.sh
+++ tools/ci-cmake.sh
@@ -1,6 +1,9 @@
+#TODO: manage LD_LIBRARY_PATH with CMake and rpath
+
 mkdir -p build_cmake \
 &&  cd build_cmake \
 &&  cmake -D CMAKE_INSTALL_PREFIX=/builds/freefem-source-feature-cmake .. \
 &&  make -j 8 \
 &&  make install \
+&&  export LD_LIBRARY_PATH=/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/build_cmake/example++-load \
 &&  make test CTEST_OUTPUT_ON_FAILURE=On 
