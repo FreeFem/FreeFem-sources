b48f693b976a3acb4529308571f3c93ca4b160c3
Cedric Doucet <cedric.doucet@inria.fr>
2018-07-18 14:56:11 +0200

    Fix typo

--- tools/ci-cmake.sh
+++ tools/ci-cmake.sh
@@ -1,9 +1,11 @@
 #TODO: manage LD_LIBRARY_PATH with CMake and rpath
+#TODO: LD_LIBRARY_PATH is added to load msh3.so from examples++-load but it used by edp files coming from examples++-3d (e.g. sphere6.edp)
+#TODO: msh3 should be removed from examples++-load and put in a common directory for all examples
 
 mkdir -p build_cmake \
 &&  cd build_cmake \
 &&  cmake -D CMAKE_INSTALL_PREFIX=/builds/freefem-source-feature-cmake .. \
 &&  make -j 8 \
 &&  make install \
-&&  export LD_LIBRARY_PATH=/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/build_cmake/example++-load \
+&&  export LD_LIBRARY_PATH=/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/build_cmake/examples++-load \
 &&  make test CTEST_OUTPUT_ON_FAILURE=On 
