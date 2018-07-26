2c13dc4d31f939212f2792ba117fef7a1c650512
Cedric Doucet <cedric.doucet@inria.fr>
2018-07-26 16:36:08 +0200

    Restrict CI tests to examples++

--- tools/ci-cmake.sh
+++ tools/ci-cmake.sh
@@ -3,18 +3,20 @@
 #TODO: msh3 should be removed from examples++-load and put in a common directory for all examples
 
 mkdir -p build_cmake \
-&&  cd build_cmake \
-&&  cmake -D CMAKE_INSTALL_PREFIX=/builds/freefem-source-feature-cmake .. \
-&&  make -j 8 \
-&&  make install \
-&&  export LD_LIBRARY_PATH=/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/build_cmake/examples++-load \
-&&  export FF_INCLUDE_PATH=$FF_INCLUDEPATH:/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/examples++ \
-&&  export FF_INCLUDE_PATH=$FF_INCLUDEPATH:/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/examples++-3d \
-&&  export FF_INCLUDE_PATH=$FF_INCLUDEPATH:/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/examples++-bug \
-&&  export FF_INCLUDE_PATH=$FF_INCLUDEPATH:/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/examples++-chapt3 \
-&&  export FF_INCLUDE_PATH=$FF_INCLUDEPATH:/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/examples++-eigen \
-&&  export FF_INCLUDE_PATH=$FF_INCLUDEPATH:/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/examples++-load \
-&&  export FF_INCLUDE_PATH=$FF_INCLUDEPATH:/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/examples++-mpi \
-&&  export FF_INCLUDE_PATH=$FF_INCLUDEPATH:/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/examples++-other \
-&&  export FF_INCLUDE_PATH=$FF_INCLUDEPATH:/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/examples++-tutorial \
-&&  make test CTEST_OUTPUT_ON_FAILURE=On 
+&& cd build_cmake \
+&& cmake -D CMAKE_INSTALL_PREFIX=/builds/freefem-source-feature-cmake .. \
+&& make -j 8 \
+&& make install \
+&& export LD_LIBRARY_PATH=/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/build_cmake/examples++-load \
+&& export FF_INCLUDE_PATH=$FF_INCLUDEPATH:/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/examples++ \
+&& cd /builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/build_cmake/examples++ \
+&& make test CTEST_OUTPUT_ON_FAILURE=On 
+#&& export FF_INCLUDE_PATH=$FF_INCLUDEPATH:/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/examples++-3d \
+#&& export FF_INCLUDE_PATH=$FF_INCLUDEPATH:/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/examples++-bug \
+#&& export FF_INCLUDE_PATH=$FF_INCLUDEPATH:/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/examples++-chapt3 \
+#&& export FF_INCLUDE_PATH=$FF_INCLUDEPATH:/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/examples++-eigen \
+#&& export FF_INCLUDE_PATH=$FF_INCLUDEPATH:/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/examples++-load \
+#&& export FF_INCLUDE_PATH=$FF_INCLUDEPATH:/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/examples++-mpi \
+#&& export FF_INCLUDE_PATH=$FF_INCLUDEPATH:/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/examples++-other \
+#&& export FF_INCLUDE_PATH=$FF_INCLUDEPATH:/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll/examples++-tutorial \
+#&& make test CTEST_OUTPUT_ON_FAILURE=On 
