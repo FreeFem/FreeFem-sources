504b5dccffd7f7c2a018fcbfe7e56d848fefaefc
Simon Garnotel <simon.garnotel@gmail.com>
2019-02-21 09:49:16 +0100

    remove examples++-* directories

--- examples++-hpddm/SLEPc-complex.cpp
+++
@@ -1,8 +0,0 @@
-//ff-c++-LIBRARY-dep: cxx11 hpddm    slepccomplex [mkl|blas] mpi
-//ff-c++-cpp-dep:
-
-#define WITH_slepccomplex
-#define  PETScandSLEPc 1
-#include "PETSc-code.hpp"
-#include "SLEPc-code.hpp"
-LOADFUNC(Init)
--- examples++-hpddm/SLEPc.cpp
+++
@@ -1,8 +0,0 @@
-//ff-c++-LIBRARY-dep: cxx11 hpddm slepc [mkl|blas] mpi
-//ff-c++-cpp-dep:
-
-#define WITH_slepc
-#define  PETScandSLEPc 1
-#include "PETSc-code.hpp"
-#include "SLEPc-code.hpp"
-LOADFUNC(Init)
