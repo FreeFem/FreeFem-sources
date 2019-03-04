60342ffcc7f2f048f01dfac504bbe511073340d2
Cedric Doucet <cedric.doucet@inria.fr>
2019-03-04 14:23:24 +0100

    Continue tests in case of failures

--- tools/ci-cmake.sh
+++ tools/ci-cmake.sh
@@ -3,28 +3,27 @@
 #TODO: msh3 should be removed from examples++-load and put in a common directory for all examples
 
 export FF_ROOT=/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll 
-export FF_LOADPATH=$FF_ROOT/build_cmake/examples++-load 
-export LD_LIBRARY_PATH=$FF_ROOT/build_cmake/examples++-load 
+#export FF_LOADPATH=$FF_ROOT/build_cmake/examples++-load 
+#export LD_LIBRARY_PATH=$FF_ROOT/build_cmake/examples++-load 
 export FF_INCLUDEPATH=$FF_ROOT/idp 
 
 mkdir -p build_cmake \
 && cd build_cmake \
 && cmake -D CMAKE_INSTALL_PREFIX=/builds/freefem-source-feature-cmake .. \
 && make -j 8 \
-&& make install \
-&& cd $FF_ROOT/build_cmake/examples/tutorial \
-&& make test CTEST_OUTPUT_ON_FAILURE=On \
-&& cd $FF_ROOT/build_cmake/examples/3d \
-&& make test CTEST_OUTPUT_ON_FAILURE=On \
-&& cd $FF_ROOT/build_cmake/examples/eigen \
-&& make test CTEST_OUTPUT_ON_FAILURE=On \
-&& cd $FF_ROOT/build_cmake/examples/mpi \
-&& make test CTEST_OUTPUT_ON_FAILURE=On \
-&& cd $FF_ROOT/build_cmake/examples/other \
-&& make test CTEST_OUTPUT_ON_FAILURE=On \
-&& cd $FF_ROOT/build_cmake/examples/tutorial \
-&& make test CTEST_OUTPUT_ON_FAILURE=On \
-&& cd $FF_ROOT/build_cmake/examples/bug \
-&& make test CTEST_OUTPUT_ON_FAILURE=On 
+&& make install 
+
+cd $FF_ROOT/build_cmake/examples/bug 
+make test CTEST_OUTPUT_ON_FAILURE=On 
+cd $FF_ROOT/build_cmake/examples/3d 
+make test CTEST_OUTPUT_ON_FAILURE=On 
+cd $FF_ROOT/build_cmake/examples/eigen 
+make test CTEST_OUTPUT_ON_FAILURE=On 
+cd $FF_ROOT/build_cmake/examples/mpi 
+make test CTEST_OUTPUT_ON_FAILURE=On 
+cd $FF_ROOT/build_cmake/examples/other 
+make test CTEST_OUTPUT_ON_FAILURE=On 
+cd $FF_ROOT/build_cmake/examples/tutorial 
+make test CTEST_OUTPUT_ON_FAILURE=On 
  
  
