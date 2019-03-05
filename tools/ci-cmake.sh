f80612d922c16be7ca8c7a80462e071f6aaa676b
Cedric Doucet <cedric.doucet@inria.fr>
2019-03-05 10:25:23 +0100

    get root directory from pwd

--- tools/ci-cmake.sh
+++ tools/ci-cmake.sh
@@ -2,9 +2,11 @@
 #TODO: LD_LIBRARY_PATH is added to load msh3.so from examples++-load but it used by edp files coming from examples++-3d (e.g. sphere6.edp)
 #TODO: msh3 should be removed from examples++-load and put in a common directory for all examples
 
-export FF_ROOT=/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll 
+#export FF_ROOT=/builds/workspace/FreeFem-source-feature-cmake-UbuntuAll 
 #export FF_LOADPATH=$FF_ROOT/build_cmake/examples++-load 
 #export LD_LIBRARY_PATH=$FF_ROOT/build_cmake/examples++-load 
+
+export FF_ROOT=$(pwd)
 export FF_INCLUDEPATH=$FF_ROOT/idp 
 
 mkdir -p build_cmake \
