737bf4ea9209dea3c5f0a8ff5b6c25aba6ffc4e5
Cedric Doucet <cedric.doucet@inria.fr>
2019-03-08 10:45:01 +0100

    Refactoring

--- cmake/modules/ff_configure_cmake.cmake
+++ cmake/modules/ff_configure_cmake.cmake
@@ -1,12 +1,14 @@
 function(ff_configure_cmake)
 
+  # make sure that "make" will run in parallel 
   include(ProcessorCount)
   
   ProcessorCount(numprocs)
   
   if(NOT numprocs EQUAL 0)
-    SET(CMAKE_BUILD_FLAGS -j ${numprocs})
-  ENDIF()
+    set(CMAKE_BUILD_FLAGS -j ${numprocs})
+  endif()
+
 
 
 endfunction(ff_configure_cmake)
