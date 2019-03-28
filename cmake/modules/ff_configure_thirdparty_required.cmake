08321261a78ff00b8b05a44e189945df2dd02795
Cedric Doucet <cedric.doucet@inria.fr>
2019-03-28 14:29:58 +0100

    Add suitesparse as a required package on windows

--- cmake/modules/ff_configure_thirdparty_required.cmake
+++ cmake/modules/ff_configure_thirdparty_required.cmake
@@ -8,24 +8,14 @@
                              #OpenGL
                              #GLUT
                              GSL
-                             HDF5)
+                             HDF5
+                             SUITESPARSE)
 
-  # SuiteSparse is systematically downloaded on Windows
-  # because the associated pacman package
-  # is not compiled in the expected way
-  if(MINGW)
-    list(APPEND FF_THIRDPARTY_DOWNLOAD SUITESPARSE)
-  else()
-    list(APPEND FF_THIRDPARTY_REQUIRED SUITESPARSE)
-  endif(MINGW)
-  
   foreach(PACKAGE ${FF_THIRDPARTY_REQUIRED})
     ff_find_package(${PACKAGE} REQUIRED)
     if(NOT FF_${PACKAGE}_FOUND)
       message(SEND_ERROR "Required package ${PACKAGE} is missing")
     endif(NOT FF_${PACKAGE}_FOUND)
   endforeach(PACKAGE ${FF_THIRDPARTY_REQUIRED})
-
-  ff_install_package(SUITESPARSE)
 
 endmacro(ff_configure_thirdparty_required)
