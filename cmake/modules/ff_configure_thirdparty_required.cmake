de9221d678daa59bf7076ffbf5cca7d71bc77ee0
Cedric Doucet <cedric.doucet@inria.fr>
2019-03-28 16:10:26 +0100

    Link fflib against metis cam and ccolamd

--- cmake/modules/ff_configure_thirdparty_required.cmake
+++ cmake/modules/ff_configure_thirdparty_required.cmake
@@ -4,6 +4,8 @@
 
   set(FF_THIRDPARTY_REQUIRED AMD
                              ARPACK
+                             CAMD
+                             CCOLAMD
                              CHOLMOD
                              COLAMD
                              DLOPEN
