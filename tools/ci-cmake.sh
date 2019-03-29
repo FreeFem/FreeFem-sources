637c42d1fb5b5e5da4926f705b1ba8c5914ee1c8
Cedric Doucet <cedric.doucet@inria.fr>
2019-03-29 10:27:22 +0100

    No more failure tolerance in CI script

--- tools/ci-cmake.sh
+++ tools/ci-cmake.sh
@@ -13,17 +13,16 @@ mkdir -p build_cmake \
 && cd build_cmake \
 && cmake -D CMAKE_CXX_COMPILER=g++ -D CMAKE_C_COMPILER=gcc -D CMAKE_INSTALL_PREFIX=/builds/freefem-source-feature-cmake .. \
 && make -j 8 VERBOSE=1 \
-&& make install
-
-cd $FF_ROOT/build_cmake/examples/3d 
-make test CTEST_OUTPUT_ON_FAILURE=On 
-cd $FF_ROOT/build_cmake/examples/eigen 
-make test CTEST_OUTPUT_ON_FAILURE=On 
-cd $FF_ROOT/build_cmake/examples/mpi 
-make test CTEST_OUTPUT_ON_FAILURE=On 
-cd $FF_ROOT/build_cmake/examples/other 
-make test CTEST_OUTPUT_ON_FAILURE=On 
-cd $FF_ROOT/build_cmake/examples/tutorial 
-make test CTEST_OUTPUT_ON_FAILURE=On 
+&& make install \
+&& cd $FF_ROOT/build_cmake/examples/3d \
+&& make test CTEST_OUTPUT_ON_FAILURE=On \
+&& cd $FF_ROOT/build_cmake/examples/eigen \ 
+&& make test CTEST_OUTPUT_ON_FAILURE=On \
+&& cd $FF_ROOT/build_cmake/examples/mpi \
+&& make test CTEST_OUTPUT_ON_FAILURE=On \
+&& cd $FF_ROOT/build_cmake/examples/other \ 
+&& make test CTEST_OUTPUT_ON_FAILURE=On \
+&& cd $FF_ROOT/build_cmake/examples/tutorial \
+&& make test CTEST_OUTPUT_ON_FAILURE=On 
  
  
