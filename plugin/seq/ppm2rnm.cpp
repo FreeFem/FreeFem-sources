e9709fa28fb7cea314463b5f7fb054098c0b841b
Simon Garnotel <simon.garnotel@gmail.com>
2019-04-19 10:46:21 +0200

    update ppm2rnm.cpp ppmimg.h ppm2rnm.edp

--- plugin/seq/ppm2rnm.cpp
+++ plugin/seq/ppm2rnm.cpp
@@ -14,7 +14,7 @@
 /* You should have received a copy of the GNU Lesser General Public License */
 /* along with FreeFem++. If not, see <http://www.gnu.org/licenses/>.        */
 /****************************************************************************/
-// SUMMARY : Tools to read ppm file
+// SUMMARY : Tools to read, save, ... ppm file
 // LICENSE : LGPLv3
 // ORG     : LJLL Universite Pierre et Marie Curie, Paris, FRANCE
 // AUTHORS : ...
@@ -42,7 +42,13 @@ typedef KN<double> *pRn;
 typedef string *pstring;
 #include "ppmimg.h"
 
+/*!
+ * \brief Information output message
+ */
 #define DISP_INFO "PPM2RMN:"
+/*!
+ * \brief Error output message
+ */
 #define DISP_ERROR "PPM2RNM - ERROR:"
 
 #ifdef __cplusplus
@@ -51,12 +57,11 @@ extern "C" {
 
 /*!
  * \brief Load a PPM image
- * \param imgname PPM image file name
- * \param type PPM image type
+ * \param imgName PPM image file name
  * \param quiet Verbosity
  * \return Success: loaded PPM image, Error: 0
  */
-PPMimage *loadPPM (const char *imgname, ubyte *type, ubyte quiet) {
+PPMimage *load_PPM (const char *imgName, ubyte quiet) {
 	pPPMimage result;
 	FILE *fp;
 	int typimg, ret, s, maxval, bitsize;
@@ -64,11 +69,11 @@ PPMimage *loadPPM (const char *imgname, ubyte *type, ubyte quiet) {
 	char buff[1024], data[256];
 
 	/* search for image */
-	fprintf(stdout, "%s Loading image: %s\n", DISP_INFO, imgname);
-	ptr = strstr(imgname, ".ppm");
-	strcpy(data, imgname);
+	fprintf(stdout, "%s Loading image: %s\n", DISP_INFO, imgName);
+	ptr = strstr(imgName, ".ppm");
+	strcpy(data, imgName);
 	if (!ptr) {
-		ptr = strstr(imgname, ".pgm");
+		ptr = strstr(imgName, ".pgm");
 		if (!ptr) {strcat(data, ".ppm");}
 	}
 
@@ -83,12 +88,14 @@ PPMimage *loadPPM (const char *imgname, ubyte *type, ubyte quiet) {
 
 	if (!fgets(buff, sizeof(buff), fp)) {
 		fprintf(stderr, "%s INVALID HEADER.\n", DISP_ERROR);
+		fclose(fp);
 		return 0;
 	}
 
 	/* check header file */
 	if (buff[0] != 'P') {
 		fprintf(stderr, "%s INVALID IMAGE FORMAT (MUST BE 'PX').\n", DISP_ERROR);
+		fclose(fp);
 		return 0;
 	}
 
@@ -197,15 +204,15 @@ PPMimage *loadPPM (const char *imgname, ubyte *type, ubyte quiet) {
 
 	fclose(fp);
 
-	if (*type == DEFAULT) {
+	if (result->type == DEFAULT) {
 		if (typimg == P2 || typimg == P5) {
-			*type = GREY;
+			result->type = GREY;
 		} else {
-			*type = COLOR;
+			result->type = COLOR;
 		}
 	}
 	/* convert to grey levels */
-	else if (*type == GREY && (typimg == P3 || typimg == P6)) {
+	else if (result->type == GREY && (typimg == P3 || typimg == P6)) {
 		int i, k;
 		fprintf(stdout, "%s Converting to grey levels\n", DISP_INFO);
 
@@ -224,31 +231,30 @@ PPMimage *loadPPM (const char *imgname, ubyte *type, ubyte quiet) {
 
 /*!
  * \brief Save a PPM image
- * \param imgname PPM image file name
+ * \param imgName PPM image file name
  * \param img PPM image
- * \param typimg PPM image type
  * \return Success: 1, Error: 0
  */
-int savePPM (const char *imgname, pPPMimage img, int typimg) {
+int save_PPM (const char *imgName, pPPMimage img) {
 	FILE *out;
 	int bitsize;
 
 	/* open file */
-	out = fopen(imgname, "w");
+	out = fopen(imgName, "w");
 	if (!out) {
-		fprintf(stderr, "%s UNABLE TO OPEN FILE %s.\n", DISP_ERROR, imgname);
+		fprintf(stderr, "%s UNABLE TO OPEN FILE %s.\n", DISP_ERROR, imgName);
 		return 0;
 	}
 
 	/* write out image file */
 	bitsize = img->sizeX * img->sizeY;
 
-	switch (typimg) {
+	switch (img->type) {
 		case P2:
 			int i, c;
 
 			fprintf(out, "P2\n");
-			fprintf(out, "# CREATOR: QIZIP Version 1, Rev. 2/2003, (c) INRIA\n");
+			fprintf(out, "# CREATOR: FreeFEM\n");
 			fprintf(out, "%d %d\n", img->sizeX, img->sizeY);
 			fprintf(out, "255\n");
 			c = 0;
@@ -265,14 +271,14 @@ int savePPM (const char *imgname, pPPMimage img, int typimg) {
 			break;
 		case P5:
 			fprintf(out, "P5\n");
-			fprintf(out, "# CREATOR: QIZIP Version 1, Rev. 2/2003, (c) INRIA\n");
+			fprintf(out, "# CREATOR: FreeFEM\n");
 			fprintf(out, "%d %d\n", img->sizeX, img->sizeY);
 			fprintf(out, "255\n");
 			fwrite(img->data, sizeof(ubyte), bitsize, out);
 			break;
 		case P6:
 			fprintf(out, "P6\n");
-			fprintf(out, "# CREATOR: QIZIP Version 1, Rev. 2/2003, (c) INRIA\n");
+			fprintf(out, "# CREATOR: FreeFEM\n");
 			fprintf(out, "%d %d\n", img->sizeX, img->sizeY);
 			fprintf(out, "255\n");
 			fwrite(img->data, sizeof(ubyte), 3 * bitsize, out);
@@ -287,30 +293,29 @@ int savePPM (const char *imgname, pPPMimage img, int typimg) {
 /*! \brief Compute difference between two images
  * \param bits First PPM image
  * \param img Second PPM image
- * \param itype PPM image type
  * \return Success: Difference PPM image, Error: 0
  */
-pPPMimage diffImg (pPPMimage bits, pPPMimage img, ubyte itype) {
-	pPPMimage dif;
+pPPMimage diff_PPM (pPPMimage bits, pPPMimage img) {
+	pPPMimage diff;
 	double psnr;
 	int i, bitsize, dmax;
 
 	fprintf(stdout, "%s Image difference\n", DISP_INFO);
 	bitsize = (int)bits->sizeX * bits->sizeY;
-	if (itype == COLOR) {bitsize *= 3;}
+	if (img->type == COLOR) {bitsize *= 3;}
 
-	dif = (PPMimage *)malloc(sizeof(PPMimage));
-	if (!dif) {
+	diff = (PPMimage *)malloc(sizeof(PPMimage));
+	if (!diff) {
 		fprintf(stderr, "%s Sorry, not enough memory. Bye.\n", DISP_ERROR);
 		return 0;
 	}
 
-	dif->sizeX = bits->sizeX;
-	dif->sizeY = bits->sizeY;
-	dif->data = (ubyte *)malloc(bitsize * sizeof(ubyte));
-	if (!dif->data) {
+	diff->sizeX = bits->sizeX;
+	diff->sizeY = bits->sizeY;
+	diff->data = (ubyte *)malloc(bitsize * sizeof(ubyte));
+	if (!diff->data) {
 		fprintf(stderr, "%s Sorry, not enough memory. Bye.\n", DISP_ERROR);
-		free(dif);
+		free(diff);
 		return 0;
 	}
 
@@ -321,7 +326,7 @@ pPPMimage diffImg (pPPMimage bits, pPPMimage img, ubyte itype) {
 		double dd = abs((int)(bits->data[i] - img->data[i]));
 		dmax = max(dmax, dd);
 		psnr += (double)dd * dd;
-		dif->data[i] = (ubyte)(255 - dd);
+		diff->data[i] = (ubyte)(255 - dd);
 	}
 
 	if (psnr == 0.0f) {fprintf(stderr, "%s PSNR problem!", DISP_ERROR);} else {
@@ -331,7 +336,14 @@ pPPMimage diffImg (pPPMimage bits, pPPMimage img, ubyte itype) {
 
 	fprintf(stdout, "%s PSNR = %.2f - dmax = %d\n", DISP_INFO, psnr, dmax);
 
-	return dif;
+	return diff;
+}
+
+void freePPMimage(pPPMimage &image) {
+	if (image) {
+		if (image->data) free(image->data);
+		free(image);
+	}
 }
 
 #ifdef __cplusplus
@@ -344,23 +356,22 @@ pPPMimage diffImg (pPPMimage bits, pPPMimage img, ubyte itype) {
  * \param b PPM image file name
  * \return Success: loaded PPM image, Error: a
  */
-pRnm read_image (pRnm const &a, const pstring &b) {
-	ubyte type, quiet, *dd;
+pRnm readPPM (pRnm const &a, const pstring &imgName) {
+	ubyte quiet, *dd;
 	int k, n, m;
 	double *mm;
 	PPMimage *image;
 
 	quiet = 1;
-	image = loadPPM(b->c_str(), &type, quiet);
-
+	image = load_PPM(imgName->c_str(), quiet);
 	if (!image) {
-		std::cerr << DISP_ERROR << " Error loadPPM image " << *b << endl;
-		CompileError((string)DISP_ERROR + " Error loadPPM image ");
+		cerr << DISP_ERROR << " Error load PPM image " << *imgName << endl;
+		CompileError((string)DISP_ERROR + " Error load PPM image ");
 		return a;
 	}
 
 	if (verbosity)
-		cout << DISP_INFO << " Size of image : " << image->sizeX << " x " << image->sizeY << " - Type = " << (int)type << endl;
+		cout << DISP_INFO << " Image size: " << image->sizeX << " x " << image->sizeY << " - Type: " << (int)image->type << endl;
 
 	n = image->sizeX;
 	m = image->sizeY;
@@ -374,9 +385,202 @@ pRnm read_image (pRnm const &a, const pstring &b) {
 			*mm++ = (double)dd[k++] / 256.;
 
 	assert(k == n * m);
-	free(image->data);
-	free(image);
+	freePPMimage(image);
 	return a;
+}
+
+/*!
+ * \brief Read PPM image
+ * \param imgName PPM image file name
+ * \return Success: loaded PPM image, Error: 0
+ */
+KNM<double> *readPPM (const pstring &imgName) {
+	ubyte quiet;
+	PPMimage *image;
+	KNM<double> *imageArray;
+
+	quiet = 1;
+	image = load_PPM(imgName->c_str(), quiet);
+
+	if (!image) {
+		cerr << DISP_ERROR << " Error loadPPM image " << *imgName << endl;
+		CompileError((string)DISP_ERROR + " Error loadPPM image ");
+		return 0;
+	}
+
+	if (verbosity)
+		cout << DISP_INFO << " Image size: " << image->sizeX << " x " << image->sizeY << " - Type: " << (int)image->type << endl;
+
+	imageArray = PPMimage2Rnm(image);
+	if (!imageArray) {
+		cerr << DISP_ERROR << " Error convert PPM image to array - " << *imgName << endl;
+		CompileError((string)DISP_ERROR + " Error convert PPM image to array - " + *imgName);
+		return 0;
+	}
+
+	freePPMimage(image);
+	return imageArray;
+}
+
+/*!
+ * \brief Save PPM image
+ * \param imgName PPM image file name
+ * \param img PPM image
+ * \return Success: true, Error: false
+ */
+bool savePPM (const pstring &imgName, pRnm const &array) {
+	int ret, k;
+	ubyte *dd;
+	double *mm;
+	PPMimage *image;
+
+	image = Rnm2PPMimage(array);
+	if (!image) {
+		cerr << DISP_ERROR << " Error convert array to PPM image - " << *imgName << endl;
+		CompileError((string)DISP_ERROR + " Error convert array to PPM image - " + *imgName);
+		return 0;
+	}
+
+	ret = save_PPM (imgName->c_str(), image);
+	if (!ret) {
+		cerr << DISP_ERROR << " Error save PPM " << *imgName << endl;
+		CompileError((string)DISP_ERROR + " Error save PPM " + *imgName);
+		freePPMimage(image);
+		return false;
+	}
+
+	freePPMimage(image);
+	return true;
+}
+
+/*!
+ * PPM image difference
+ * \param PPM image 1
+ * \param PPM image 2
+ * \return Success: image difference, Error: 0
+ */
+pRnm diffPPM (pRnm const &img1, pRnm const &img2) {
+	PPMimage *image1, *image2, *imageDiff;
+	KNM<double> *imgDiff;
+
+	image1 = Rnm2PPMimage(img1);
+	if (!image1) {
+		cerr << DISP_ERROR << " Error convert array to PPM image" << endl;
+		CompileError((string)DISP_ERROR + " Error convert array to PPM image");
+		return 0;
+	}
+
+	image2 = Rnm2PPMimage(img2);
+	if (!image2) {
+		cerr << DISP_ERROR << " Error convert array to PPM image" << endl;
+		CompileError((string)DISP_ERROR + " Error convert array to PPM image");
+		return 0;
+	}
+
+	imageDiff = diff_PPM (image1, image2);
+	if (!imageDiff) {
+		cerr << DISP_ERROR << " Error image difference - " << endl;
+		CompileError((string)DISP_ERROR + " Error image difference");
+		return 0;
+	}
+
+	imgDiff = PPMimage2Rnm(imageDiff);
+	if (!image2) {
+		cerr << DISP_ERROR << " Error convert PPM image to array" << endl;
+		CompileError((string)DISP_ERROR + " Error convert PPM image to array");
+		return 0;
+	}
+
+	freePPMimage(image1);
+	freePPMimage(image2);
+	freePPMimage(imageDiff);
+
+	return imgDiff;
+}
+
+/*!
+ * \brief Convert PPM image to array
+ * \param img PPM image
+ * \return Success: Array, Error: 0
+ */
+pRnm PPMimage2Rnm (pPPMimage const &img) {
+	ubyte *u;
+	int k, n, m;
+	double *d;
+	KNM<double> *array;
+
+	array = new KNM<double>;
+	if (!array) {
+		cerr << DISP_ERROR << " Error allocate memory" << endl;
+		CompileError((string)DISP_ERROR + " Error allocate memory");
+		return 0;
+	}
+
+	n = (int)img->sizeX;
+	m = (int)img->sizeY;
+	array->init(n, m);
+
+	u = img->data;
+	k = 0;
+	d = *array;
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < m; ++j)
+			*d++ = (double)u[k++] / 256.;
+
+	if (k != n*m) {
+		cerr << DISP_ERROR << " PPM image to array error"<< endl;
+		CompileError((string)DISP_ERROR + " PPM image to array error");
+		return 0;
+	}
+
+	return array;
+}
+
+/*!
+ * \brief Convert array to PPM image
+ * \param array Array
+ * \return Success: PPM image, Error: 0
+ */
+pPPMimage Rnm2PPMimage (pRnm const &array) {
+	ubyte *u;
+	short n, m;
+	int k;
+	double *d;
+	PPMimage *image;
+
+	image = new PPMimage;
+	if (!image) {
+		cerr << DISP_ERROR << " Error allocate memory"<< endl;
+		CompileError((string)DISP_ERROR + " Error allocate memory");
+		return 0;
+	}
+
+	n = (short)array->N();
+	m = (short)array->M();
+	image->sizeX = n;
+	image->sizeY = m;
+	image->type = (ubyte)P2;
+	image->data = (ubyte *)malloc(image->sizeX * image->sizeY * sizeof(ubyte));
+	if (!image->data) {
+		cerr << DISP_ERROR << " Error allocate memory"<< endl;
+		CompileError((string)DISP_ERROR + " Error allocate memory");
+		return 0;
+	}
+
+	k = 0;
+	u = image->data;
+	d = *array;
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < m; ++j)
+			*u++ = (ubyte)(d[k++]*256.);
+
+	if (k != n*m) {
+		cerr << DISP_ERROR << " Array to PPM image error"<< endl;
+		CompileError((string)DISP_ERROR + " Array to PPM image error");
+		return 0;
+	}
+
+	return image;
 }
 
 /*!
@@ -394,14 +598,14 @@ pRn seta (pRn const &a, const pRnm &b) {
  * \brief Dynamic load
  */
 static void Load_PPM2RNM () {
-	cout << " load: ppm2rmn" << endl;
+	cout << " load: ppm2rnm" << endl;
 
-	TheOperators->Add("<-",
-	                  new OneOperator2_<KNM<double> *, KNM<double> *, string *>(&read_image)
-	                  );
-	TheOperators->Add("=",
-	                  new OneOperator2_<KN<double> *, KN<double> *, KNM<double> *>(seta)
-	                  );
+	TheOperators->Add("<-", new OneOperator2_<KNM<double>*, KNM<double>*, string*>(&readPPM));
+	TheOperators->Add("=", new OneOperator2_<KN<double>*, KN<double>*, KNM<double>*>(seta));
+
+	Global.Add("readPPM", "(", new OneOperator1_<KNM<double>*, string*>(&readPPM));
+	Global.Add("savePPM", "(", new OneOperator2_<bool, string*, KNM<double>*>(&savePPM));
+	Global.Add("diffPPM", "(", new OneOperator2_<KNM<double>*, KNM<double>*, KNM<double>*>(&diffPPM));
 }
 
 LOADFUNC(Load_PPM2RNM)
