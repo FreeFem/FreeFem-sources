7d105d093b84fed709a7e58092230acabffe0dd0
QuentinTessier <quentin.tessier@epitech.eu>
2019-07-23 13:10:16 +0200

    Updating ffPacket to be able to serialize any type of data sent with the server

--- src/ffgraphics/server/ffPacket.hpp
+++ src/ffgraphics/server/ffPacket.hpp
@@ -5,59 +5,47 @@
 #include <nlohmann/json.hpp>
 #include <string>
 #include <vector>
+#include "RNM.hpp"
 
-struct ffPacket {
-    std::string m_Header;
-    std::vector<uint8_t> m_Data;
+class ffPacket {
+    public:
 
-    void display_content() {
-        std::cout << m_Header << "\n";
-        for (size_t i = 0; i < m_Data.size(); ++i) {
-            std::cout << m_Data[i];
+        inline std::string& GetHeader() const { return m_Header; }
+
+        inline std::vector<uint8_t>& GetPacketData() const { return m_Data; }
+
+        ostream& operator<<(ostream& os, const ffPacket& packet) {
+            os << "Header :\n" << packet.GetHeader() << "\n";
+            const std::vector<uint8_t>& tmp = packet.GetPacketData();
+            for (int i = 0; i < tmp.size(); ++i) {
+                os << tmp[i];
+            }
+            os << "\n";
         }
-        std::cout << "\n";
-    }
-};
 
-class ffSerializer {
-    using json = nlohmann::json; // Quality of life using
+    private:
 
-  public:
-    /* Build the basic header data */
-    ffSerializer() {
-        m_Header["Size"] = 0;
-        m_Header["Version"] = "FreeFem++ Header 0.1";
-        m_Header["Padding"] = "";
-    }
+        void GetPacketHeader(int size)
+        {
+            json j;
+            std::string tmp;
 
-    ffPacket CreatePacket(json data) {
-        // Compress the JSON using the cbor algorithm
-        std::vector<uint8_t> compressed = json::to_cbor(data);
-        size_t size = compressed.size();
-        std::string header;
-        ffPacket packet;
-
-        // Adding the real size of the message to the Header
-        m_Header["Size"] = size;
-        // Computing the padding necessary to have a Header of 66 bytes
-        header = m_Header.dump();
-        if (header.length() < 66) {
-            std::string padding = "";
-            for (size_t i = 0; i < 66 - header.length(); ++i)
-                padding.push_back('a');
-            m_Header["Padding"] = padding;
+            j["Size"] = size;
+            j["Version"] = "FreeFem++ Header 0.1";
+            j["Padding"] = "";
+            tmp = j.dump();
+            if (tmp.length() < 66) {
+                std::string padding = "";
+                for (size_t i = 0; i < 66 - tmp.length(); ++i)
+                    padding.push_back('a');
+                j["Padding"] = padding;
+            }
+            m_Header = j.dump();
         }
-        // Create the packet from it's header and packet
-        packet.m_Data = compressed;
-        packet.m_Header = m_Header.dump();
-        packet.m_Header += "\n";
-        return packet;
-    }
 
-    void AddData(json::value_type& vType, double data) { vType += data; }
+        std::string m_Header;
+        std::vector<uint8_t> m_Data;
 
-  private:
-    json m_Header;
 };
 
 #endif // FF_PACKET_HPP
