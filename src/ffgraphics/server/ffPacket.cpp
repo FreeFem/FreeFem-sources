3abb59dd4dfd73fa9480682237732229d8bf062d
QuentinTessier <quentin.tessier@epitech.eu>
2019-07-24 12:56:07 +0200

    Adding JSON format for curves

--- src/ffgraphics/server/ffPacket.cpp
+++ src/ffgraphics/server/ffPacket.cpp
@@ -1,15 +1,27 @@
 #include "ffPacket.hpp"
 #include "RNM.hpp"
 
-
+/**
+ * Transform a number of KN_<double> into JSON.
+ * Sample JSON file :
+ * {
+ *      "Curve": {
+ *          "_comment": "Dimension describe the dimension of the curve and how it's stored in JSON"
+ *          "Dimension":
+ *          "_comment": "Value is a array of double, where all data are stored in a contagious array ordered by point"
+ *          "Values": []
+ *      }
+ * }
+ */
 template<>
 void ffPacket::jsonify<std::vector<KN_<double>>>(std::vector<KN_<double>> data)
 {
-    std::string names[4] = {"x", "y", "z", "w"};
+    m_JSON["Curve"]["Dimension"] = data.size();
+    int element_nbr = data[0].size();
 
-    for (int i = 0; i < data.size(); i += 1) {
-        double *tmp = data[i].operator double *();
-        for (int j = 0; j < data[i].size(); j += 1)
-            m_JSON["Curve"][names[i]] += tmp[j];
+    for (int i = 0; i < element_nbr; i += 1) {
+        for (int j = 0; j < data.size(); j += 1) {
+            m_JSON["Curve"]["Values"] += data[j][i];
+        }
     }
 }
