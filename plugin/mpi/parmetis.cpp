90fe89a4d9aaff6e9c6f7e5aa294ed632f2921bf
Frederic Hecht <frederic.hecht@upmc.fr>
2019-08-20 15:31:21 +0200

    1)  correct problem with metis/parmetis  plugin seq and mpi 2) coorect code segfault 9 fois sur 10, encore à cause de problèmes liés à meshS… (maillage obtenu via MMG) in case on no manifold  boundary.  (Thank. P. Jolivet) 3) pass to version 4.4

--- plugin/mpi/parmetis.cpp
+++ plugin/mpi/parmetis.cpp
@@ -1,33 +1,40 @@
-// SUMMARY  :   add interface with partionning library ParMETIS
-// USAGE    : LGPL
-// ORG      : Centre National de la Recherche Scientifique
-// AUTHOR   : P. Jolivet
-// E-MAIL   : P. Jolivet <pierre.jolivet@enseeiht.fr>
+/****************************************************************************/
+/* This file is part of FreeFEM.                                            */
+/*                                                                          */
+/* FreeFEM is free software: you can redistribute it and/or modify          */
+/* it under the terms of the GNU Lesser General Public License as           */
+/* published by the Free Software Foundation, either version 3 of           */
+/* the License, or (at your option) any later version.                      */
+/*                                                                          */
+/* FreeFEM is distributed in the hope that it will be useful,               */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of           */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            */
+/* GNU Lesser General Public License for more details.                      */
+/*                                                                          */
+/* You should have received a copy of the GNU Lesser General Public License */
+/* along with FreeFEM. If not, see <http://www.gnu.org/licenses/>.          */
+/****************************************************************************/
+// SUMMARY  :   add interface with partionning library METSI and ParMETIS
+// LICENSE : LGPLv3
+// ORG     : LJLL Universite Pierre et Marie Curie, Paris, FRANCE
+// AUTHORS : Frederic Hecht, P. Jolivet
+// E-MAIL  : frederic.hecht@sorbonne-universite.fr
+// E-MAIL  :  P. Jolivet <pierre.jolivet@enseeiht.fr>
+//  merge of metis and parmetis interface (F. Hecht aug 2019)
 
-/*
- This file is part of Freefem++
-
- Freefem++ is free software; you can redistribute it and/or modify
- it under the terms of the GNU Lesser General Public License as published by
- the Free Software Foundation; either version 2.1 of the License, or
- (at your option) any later version.
-
- Freefem++  is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU Lesser General Public License for more details.
-
- You should have received a copy of the GNU Lesser General Public License
- along with Freefem++; if not, write to the Free Software
- Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
- */
-
-//ff-c++-LIBRARY-dep: mpi parmetis
+// *INDENT-OFF* //
+//ff-c++-LIBRARY-dep: [mpi parmetis|metis]
 //ff-c++-cpp-dep:
+// *INDENT-ON* //
 
 #include <ff++.hpp>
 #include <cmath>
+typedef KNM<double> *pRnm;
+typedef KN<double> *pRn;
+typedef string *pstring;
+
+#ifdef WITH_mpi
+
 #include <parmetis.h>
 #ifndef IDX_T
 #define IDX_T MPI_INT
@@ -35,17 +42,17 @@
 
 template<class Type, class Mesh>
 class ParMETIS_Op : public E_F0mps {
-    public:
-        Expression part;
-        Expression pTh;
-        Expression lparts;
-        static const int n_name_param = 2;
-        static basicAC_F0::name_and_type name_param[];
-        Expression nargs[n_name_param];
-        ParMETIS_Op(const basicAC_F0& args, Expression param1, Expression param2, Expression param3) : part(param1), pTh(param2), lparts(param3) {
-            args.SetNameParam(n_name_param, name_param, nargs);
-        }
-        AnyType operator()(Stack stack) const;
+public:
+    Expression part;
+    Expression pTh;
+    Expression lparts;
+    static const int n_name_param = 2;
+    static basicAC_F0::name_and_type name_param[];
+    Expression nargs[n_name_param];
+    ParMETIS_Op(const basicAC_F0& args, Expression param1, Expression param2, Expression param3) : part(param1), pTh(param2), lparts(param3) {
+        args.SetNameParam(n_name_param, name_param, nargs);
+    }
+    AnyType operator()(Stack stack) const;
 };
 template<class Type, class Mesh>
 basicAC_F0::name_and_type ParMETIS_Op<Type, Mesh>::name_param[] = {
@@ -54,11 +61,11 @@ basicAC_F0::name_and_type ParMETIS_Op<Type, Mesh>::name_param[] = {
 };
 template<class Type, class Mesh>
 class ParMETIS : public OneOperator {
-    public:
-        ParMETIS() : OneOperator(atype<long>(), atype<KN<Type>*>(), atype<const Mesh*>(), atype<long>()) { }
-        E_F0* code(const basicAC_F0& args) const {
-            return new ParMETIS_Op<Type, Mesh>(args, t[0]->CastTo(args[0]), t[1]->CastTo(args[1]), t[2]->CastTo(args[2]));
-        }
+public:
+    ParMETIS() : OneOperator(atype<long>(), atype<KN<Type>*>(), atype<const Mesh*>(), atype<long>()) { }
+    E_F0* code(const basicAC_F0& args) const {
+        return new ParMETIS_Op<Type, Mesh>(args, t[0]->CastTo(args[0]), t[1]->CastTo(args[1]), t[2]->CastTo(args[2]));
+    }
 };
 template<class Type, class Mesh>
 AnyType ParMETIS_Op<Type, Mesh>::operator()(Stack stack) const {
@@ -154,11 +161,141 @@ AnyType ParMETIS_Op<Type, Mesh>::operator()(Stack stack) const {
     return 0L;
 }
 
-static void Load_Init() {
-    Global.Add("parmetis", "(", new ParMETIS<long, Mesh>);
-    Global.Add("parmetis", "(", new ParMETIS<double, Mesh>);
-    Global.Add("parmetis", "(", new ParMETIS<long, Mesh3>);
-    Global.Add("parmetis", "(", new ParMETIS<double, Mesh3>);
+
+#else
+extern "C" {
+#include <metis.h>
+}
+
+#endif
+
+
+#ifdef  METIS_VER_MAJOR
+// METIS_PartMeshDual(&ne, &nn, elmnts, &etype, &numflag, &nparts, &edgecut, epart, npart);
+extern "C" {
+	real_t libmetis__ComputeElementBalance (idx_t ne, idx_t nparts, idx_t *where);
+}
+#else
+typedef idxtype idx_t;
+#endif
+template<class Mesh, int NO, typename R>
+KN<R>*partmetis (Stack s, KN<R> *const &part, Mesh *const &pTh, long const &lparts) {
+	ffassert(pTh);
+	const Mesh &Th(*pTh);
+	idx_t nt = Th.nt, nv = Th.nv;
+	idx_t nve = Mesh::RdHat::d + 1;
+
+	KN<idx_t> eptr(nt + 1), elmnts(nve * nt), epart(nt), npart(nv);
+
+	for (idx_t k = 0, i = 0; k < nt; ++k) {
+		eptr[k] = i;
+
+		for (idx_t j = 0; j < nve; j++) {
+			elmnts[i++] = Th(k, j);
+		}
+
+		eptr[k + 1] = i;
+	}
+
+	idx_t numflag = 0;
+	idx_t nparts = lparts;
+	idx_t edgecut;
+	idx_t etype = nve - 2;	// triangle or tet .  change FH fevr 2010
+	idx_t ncommon = 1;
+#ifdef  METIS_VER_MAJOR
+	if (NO == 0) {
+		METIS_PartMeshNodal(&nt, &nv, eptr, (idx_t *)elmnts, 0, 0, &nparts, 0, 0, &edgecut, (idx_t *)epart, (idx_t *)npart);
+	} else {
+		METIS_PartMeshDual(&nt, &nv, eptr, (idx_t *)elmnts, 0, 0, &ncommon, &nparts, 0, 0, &edgecut, (idx_t *)epart, (idx_t *)npart);
+	}
+
+	if (verbosity) {
+		printf("  --metisOA: %d-way Edge-Cut: %7d, Balance: %5.2f Nodal=0/Dual %d\n", nparts, nve, libmetis__ComputeElementBalance(nt, nparts, epart), NO);
+	}
+
+#else
+	if (NO == 0) {
+		METIS_PartMeshNodal(&nt, &nv, elmnts, &etype, &numflag, &nparts, &edgecut, epart, npart);
+	} else {
+		METIS_PartMeshDual(&nt, &nv, elmnts, &etype, &numflag, &nparts, &edgecut, epart, npart);
+	}
+
+	if (verbosity) {
+		printf("  --metis: %d-way Edge-Cut: %7d, Balance: %5.2f Nodal=0/Dual %d\n", nparts, nve, ComputeElementBalance(nt, nparts, epart), NO);
+	}
+
+#endif
+	part->resize(nt);
+	*part = epart;
+	return part;
+}
+
+KN<long>*partmetisd (Stack s, KN<long> *const &part, Mesh *const &pTh, long const &lparts) {
+	ffassert(pTh);
+	const Mesh &Th(*pTh);
+	idx_t nt = Th.nt, nv = Th.nv;
+	idx_t nve = Mesh::Element::NbV;
+
+	KN<idx_t> elmnts(nve * nt), epart(nt), npart(nv);
+
+	for (idx_t k = 0, i = 0; k < nt; ++k) {
+		for (idx_t j = 0; j < nve; j++) {
+			elmnts[i++] = Th(k, j);
+		}
+	}
+
+	idx_t numflag = 0;
+	idx_t nparts = lparts;
+	idx_t edgecut;
+#ifdef  METIS_VER_MAJOR
+	printf("  %d-way Edge-Cut: %7d, Balance: %5.2f\n", nparts, nve, libmetis__ComputeElementBalance(nt, nparts, epart));
+#else
+	printf("  %d-way Edge-Cut: %7d, Balance: %5.2f\n", nparts, nve, ComputeElementBalance(nt, nparts, epart));
+#endif
+	part->resize(nt);
+	*part = epart;
+	return part;
+}
+
+static void Load_Init () {
+    double metisversion = 4;
+    if (!Global.Find("metisversion").NotNull()) {
+	if (verbosity && mpirank == 0)
+#ifdef METIS_VER_MAJOR
+	{cout << " load: init metis (v  " << METIS_VER_MAJOR << " )\n";
+            metisversion = METIS_VER_MAJOR + METIS_VER_MINOR*.01;
+        }
+
+#else
+	{cout << " load: init metis (v 4 )\n";}
+#endif
+        
+        Global.New("metisversion", CConstant<double>(metisversion)); // Add ..
+	Global.Add("metisnodal", "(", new OneOperator3_<KN<long> *, KN<long> *, const Mesh *, long, E_F_stackF0F0F0_<KN<long> *, KN<long> *, const Mesh *, long> >(&partmetis<const Mesh, 0> ));
+	Global.Add("metisdual", "(", new OneOperator3_<KN<long> *, KN<long> *, const Mesh *, long, E_F_stackF0F0F0_<KN<long> *, KN<long> *, const Mesh *, long> >(&partmetis<const Mesh, 1> ));
+	Global.Add("metisnodal", "(", new OneOperator3_<KN<long> *, KN<long> *, const Mesh3 *, long, E_F_stackF0F0F0_<KN<long> *, KN<long> *, const Mesh3 *, long> >(&partmetis<const Mesh3, 0> ));
+	Global.Add("metisdual", "(", new OneOperator3_<KN<long> *, KN<long> *, const Mesh3 *, long, E_F_stackF0F0F0_<KN<long> *, KN<long> *, const Mesh3 *, long> >(&partmetis<const Mesh3, 1> ));
+	Global.Add("metisnodal", "(", new OneOperator3_<KN<long> *, KN<long> *, const MeshS *, long, E_F_stackF0F0F0_<KN<long> *, KN<long> *, const MeshS *, long> >(&partmetis<const MeshS, 0> ));
+	Global.Add("metisdual", "(", new OneOperator3_<KN<long> *, KN<long> *, const MeshS *, long, E_F_stackF0F0F0_<KN<long> *, KN<long> *, const MeshS *, long> >(&partmetis<const MeshS, 1> ));
+
+
+	Global.Add("metisnodal", "(", new OneOperator3_<KN<double> *, KN<double> *, const Mesh *, long, E_F_stackF0F0F0_<KN<double> *, KN<double> *, const Mesh *, long> >(&partmetis<const Mesh, 0> ));
+	Global.Add("metisdual", "(", new OneOperator3_<KN<double> *, KN<double> *, const Mesh *, long, E_F_stackF0F0F0_<KN<double> *, KN<double> *, const Mesh *, long> >(&partmetis<const Mesh, 1> ));
+	Global.Add("metisnodal", "(", new OneOperator3_<KN<double> *, KN<double> *, const Mesh3 *, long, E_F_stackF0F0F0_<KN<double> *, KN<double> *, const Mesh3 *, long> >(&partmetis<const Mesh3, 0> ));
+	Global.Add("metisdual", "(", new OneOperator3_<KN<double> *, KN<double> *, const Mesh3 *, long, E_F_stackF0F0F0_<KN<double> *, KN<double> *, const Mesh3 *, long> >(&partmetis<const Mesh3, 1> ));
+	Global.Add("metisnodal", "(", new OneOperator3_<KN<double> *, KN<double> *, const MeshS *, long, E_F_stackF0F0F0_<KN<double> *, KN<double> *, const MeshS *, long> >(&partmetis<const MeshS, 0> ));
+	Global.Add("metisdual", "(", new OneOperator3_<KN<double> *, KN<double> *, const MeshS *, long, E_F_stackF0F0F0_<KN<double> *, KN<double> *, const MeshS *, long> >(&partmetis<const MeshS, 1> ));
+    }
+    else if(mpirank==0 && verbosity) cout << " already load of metis  ... skeep  \n";
+#ifdef WITH_mpi
+    if (!Global.Find("parmetis").NotNull()) {
+     Global.Add("parmetis", "(", new ParMETIS<long, Mesh>);
+     Global.Add("parmetis", "(", new ParMETIS<double, Mesh>);
+     Global.Add("parmetis", "(", new ParMETIS<long, Mesh3>);
+     Global.Add("parmetis", "(", new ParMETIS<double, Mesh3>);
+    }
+    else if(mpirank==0 && verbosity) cout << " already load of parmetis  ... skeep  \n";
+#endif
 }
 
 LOADFUNC(Load_Init)
