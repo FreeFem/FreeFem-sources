fe2374d5c53d651e6981bbb07a5550b88a1ae463
Frederic Hecht <frederic.hecht@upmc.fr>
2019-08-30 09:55:03 +0200

    correct error in check size of A=0; when A is sparse matrix to set all coef =0. (trap in sparse-matrix.edp)

--- src/femlib/HashMatrix.hpp
+++ src/femlib/HashMatrix.hpp
@@ -85,7 +85,8 @@ public:
     I ntgv;
 
     I NbCoef() const {return  (I) nnz;}
-    void setcoef(const KN_<R> & x){KN_<R>c(this->aij,nnz); ffassert(x.SameShape(c));c = x;}
+    void setcoef(const KN_<R> & x){KN_<R>c(this->aij,nnz); ffassert(x.SameShape(c));
+        if( x.constant())  c=x[0];   else c = x;}
     void getcoef( KN_<R> & x) const {ffassert(x.N()==(I) nnz);x =KN_<R>(this->aij,nnz);}
 
     void setdiag(const KN_<R> & d);
