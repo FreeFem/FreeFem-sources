cdfdfcfcaec205a8f6940ff9694112e12092ff7b
Axel FOURMONT <axel.fourmont@inria.fr>
2019-09-03 18:36:20 +0200

    fix skipped test in ffddm, rebuild AutoGeneratedFile.tar.gz

--- etc/jenkins/resultForJenkins/resultForJenkins.sh
+++ etc/jenkins/resultForJenkins/resultForJenkins.sh
@@ -6,11 +6,12 @@ rm result.txt
 
 grep 'global-test-result: FAIL' ../../../examples/*/*trs  | sed -e "s/.trs::global-test-result/ /g ;  s/\//:/g ; s/ : /:/g  " | cut -c19- >> result.txt
 grep 'global-test-result: XFAIL' ../../../examples/*/*trs  | sed -e "s/.trs::global-test-result/ /g ;  s/\//:/g ; s/ : /:/g " | cut -c19- >> result.txt
+grep 'global-test-result: CPU' ../../../examples/*/*trs  | sed -e "s/.trs::global-test-result/ /g ;  s/\//:/g ; s/ : /:/g " | cut -c19- >> result.txt
 grep 'global-test-result: SKIP' ../../../examples/*/*trs  | sed -e "s/.trs::global-test-result/ /g ;  s/\//:/g ; s/ : /:/g " | cut -c19- >> result.txt
 grep 'global-test-result: PASS' ../../../examples/*/*trs  | sed -e "s/.trs::global-test-result/ /g ;  s/\//:/g ; s/ : /:/g " | cut -c19- >> result.txt
 
-tests=0 failures=0 errors=0 skipped=0 xerrors=0
-Gtests=0 Gfailures=0 Gerrors=0 Gskipped=0 Gxerrors=0
+tests=0 failures=0 errors=0 skipped=0 xerrors=0 cpu=0
+Gtests=0 Gfailures=0 Gerrors=0 Gskipped=0 Gxerrors=0 Gcpu=0
 endclass=0
 folder=toto
 first=1
@@ -39,8 +40,14 @@ elif [ "$result" == "SKIP" ]; then
      skipped=$((skipped+1))
 	 Gskipped=$((Gskipped+1))
 	 echo "        <testcase name=\"$name\" classname=\"$classname\">" >> report.xml
-	 echo "            <skipped> put a message </skipped> " >> report.xml
+	 echo "            <skipped> Skipped test, not library/plugin on the VM </skipped> " >> report.xml
 	 echo "        </testcase>" >> report.xml 
+ elif [ "$result" == "CPU" ]; then 
+      skipped=$((cpu+1))
+ 	 Gskipped=$((Gcpu+1))
+ 	 echo "        <testcase name=\"$name\" classname=\"$classname\">" >> report.xml
+ 	 echo "            <failure> Cputime limit exceeded </failure> " >> report.xml
+ 	 echo "        </testcase>" >> report.xml 	 	 
 elif [ "$result" == "XFAIL" ]; then 
      errors=$((errors+1))
 	 Gerrors=$((Gerrors+1))
@@ -63,6 +70,7 @@ echo "</testsuites>" >> report.xml
 sed -i -e "s/ffffff/    <testsuite name=\"$folder\" tests=\"$tests\" failures=\"$failures\" errors=\"$errors\" skipped=\"$skipped\" time=\"0.000\">/g" report.xml
 
 sed -i -e "s/<testsuites name=\"FreeFEM\" duration=\"0.000\">/<testsuites name=\"FreeFEM\" tests=\"$Gtests\" failures=\"$Gfailures\" errors=\"$Gerrors\" skipped=\"$Gskipped\" duration=\"0.000\">/g" report.xml
+
 echo " ****************Make check results************ "
-echo " PASS: $Gtests  SKIPPED: $Gskipped   XFAIL: $Gerrors  FAIL:$Gfailures"
-mate report.xml
+echo " PASS: $Gtests  SKIPPED: $Gskipped   XFAIL: $Gerrors  FAIL:$ Gfailures  CPU: Gcpu"
+
