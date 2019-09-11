3800e4c12f1bcd24323c13d171c7fc4e7bd7f2e3
Axel FOURMONT <axel.fourmont@inria.fr>
2019-09-11 14:59:03 +0200

    fixed problems on Jenkins CI [ci skip]

--- etc/jenkins/job3.sh
+++ etc/jenkins/job3.sh
@@ -6,7 +6,7 @@
 echo "Job 3"
 casejob=3
 # change default  compiler
-change_compiler=etc/jenkins/change_compiler-`uname -s`-`uname -r`-$casejob.sh
+change_compiler=etc/jenkins/change_compiler/change_compiler-`uname -s`-`uname -r`-$casejob.sh
 echo try to source file  "$change_compiler"
 test -f "$change_compiler" && echo  source file "$change_compiler"
 test -f "$change_compiler" && cat  "$change_compiler"
