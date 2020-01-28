2b2ffaa200892f152c647b81461b636f55ff7dae
Pierre Jolivet <pierre.jolivet@enseeiht.fr>
2020-01-28 14:36:02 +0100

    Adds interface to localParameter.
    [ci skip]

--- plugin/seq/mmg.cpp
+++ plugin/seq/mmg.cpp
@@ -226,7 +226,7 @@ template<class ffmesh>
 class mmg_Op : public E_F0mps {
  public:
   Expression eTh, xx, yy, zz;
-  static const int n_name_param = std::is_same<ffmesh,Mesh3>::value ? 26 : 19;
+  static const int n_name_param = std::is_same<ffmesh,Mesh3>::value ? 27 : 19;
   static basicAC_F0::name_and_type name_param[];
   Expression nargs[n_name_param];
 
@@ -302,7 +302,8 @@ basicAC_F0::name_and_type mmg_Op<Mesh3>::name_param[] = {
 {"hgrad"             , &typeid(double)},/*!< [val], Control gradation */
 {"ls"                , &typeid(double)},/*!< [val], Value of level-set */
 //{"rmc"               , &typeid(double)},/*!< [-1/val], Remove small connex componants in level-set mode */
-{"requiredTriangle"  , &typeid(KN<long>*)}/*!< [val], Value of level-set */
+{"requiredTriangle"  , &typeid(KN<long>*)},/*!< [val], Value of level-set */
+{"localParameter"    , &typeid(KNM<double>*)}/*!< [val], Value of level-set */
 };
 
 template<>
@@ -351,12 +352,16 @@ AnyType mmg_Op<Mesh3>::operator( )(Stack stack) const {
 
   KN< double > *pmetric = 0;
   KN< long > *prequiredTriangle = 0;
+  KNM< double > *plocalParameter = 0;
 
   if (nargs[0]) {
     pmetric = GetAny< KN< double > * >((*nargs[0])(stack));
   }
   if (nargs[25]) {
     prequiredTriangle = GetAny< KN< long > * >((*nargs[25])(stack));
+  }
+  if (nargs[26]) {
+    plocalParameter = GetAny< KNM< double > * >((*nargs[26])(stack));
   }
 
   MMG5_pMesh mesh;
@@ -417,6 +422,18 @@ AnyType mmg_Op<Mesh3>::operator( )(Stack stack) const {
           if ( MMG3D_Set_requiredTriangle(mesh,k) != 1 ) {
             exit(EXIT_FAILURE);
           }
+        }
+      }
+    }
+    if (plocalParameter && plocalParameter->M( ) > 0) {
+      const KNM< double > &localParameter = *plocalParameter;
+      ffassert(localParameter.N() == 4);
+      if ( MMG3D_Set_iparameter(mesh,sol,MMG3D_IPARAM_numberOfLocalParam,localParameter.M()) != 1 ) {
+        exit(EXIT_FAILURE);
+      }
+      for(int j = 0; j < localParameter.M(); ++j) {
+        if ( MMG3D_Set_localParameter(mesh,sol,MMG5_Triangle,localParameter(0,j),localParameter(1,j),localParameter(2,j),localParameter(3,j)) != 1 ) {
+          exit(EXIT_FAILURE);
         }
       }
     }
