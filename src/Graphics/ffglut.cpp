46acaf8cae5c1bff0b2d0c9658bec529b39319b7
Axel FOURMONT <axel.fourmont@inria.fr>
2020-02-13 21:16:18 +0100

    deformed mesh with solution in iso values case for surface FE (touch 2 in 3d view) [ci skip]

--- src/Graphics/ffglut.cpp
+++ src/Graphics/ffglut.cpp
@@ -3219,14 +3219,19 @@ void ThePlot::DrawIsoT(const R3 Pt[3],const R ff[3],const R3 Nt[3],const R * Vis
             if(((fi<=xf)&&(fj>=xf))||((fi>=xf)&&(fj<=xf))) {
                 if (Abs(fi-fj)<=0.1e-10) {    /* one side must be drawn */
                     color(l+4);
-                     if(changePlotdim){
-                    glVertex3f(Pt[i].x+NQ[i].x*xf*rapz, Pt[i].y+NQ[i].y*xf*rapz, Pt[i].z+NQ[i].z*xf*rapz);
-                    glVertex3f(Pt[j].x+NQ[j].x*xf*rapz, Pt[j].y+NQ[j].y*xf*rapz, Pt[j].z+NQ[j].z*xf*rapz);
-                     }
-                     else {
-                         glVertex3f(Pt[i].x, Pt[i].y, Pt[i].z);
-                         glVertex3f(Pt[j].x, Pt[j].y, Pt[j].z);
-                     }
+                    if(viewdim==3)
+                        if(changePlotdim){
+                            glVertex3f(Pt[i].x+NQ[i].x*xf*rapz, Pt[i].y+NQ[i].y*xf*rapz, Pt[i].z+NQ[i].z*xf*rapz);
+                            glVertex3f(Pt[j].x+NQ[j].x*xf*rapz, Pt[j].y+NQ[j].y*xf*rapz, Pt[j].z+NQ[j].z*xf*rapz);
+                        }
+                        else {
+                            glVertex3f(Pt[i].x, Pt[i].y, Pt[i].z);
+                            glVertex3f(Pt[j].x, Pt[j].y, Pt[j].z);
+                        }
+                        else if(viewdim==2) {
+                            glVertex3f(Pt[i].x, Pt[i].y, xf*rapz);
+                            glVertex3f(Pt[j].x, Pt[j].y, xf*rapz);
+                        }
                 }
                 else {
                     R  xlam=(fi-xf)/(fi-fj);
@@ -3236,18 +3241,23 @@ void ThePlot::DrawIsoT(const R3 Pt[3],const R ff[3],const R3 Nt[3],const R * Vis
             }
         }
 
-        if (im>2)
+        if (im>=2)
         {
             color(l+4);
             if( R3(PQ[0],PQ[1]).norme2() > eps2 ) {
-                if(changePlotdim){
-                    glVertex3f(PQ[0].x+NQ[0].x*xf*rapz, PQ[0].y+NQ[0].y*xf*rapz, PQ[0].z+NQ[0].z*xf*rapz);
-                    glVertex3f(PQ[1].x+NQ[1].x*xf*rapz, PQ[1].y+NQ[1].y*xf*rapz, PQ[1].z+NQ[1].z*xf*rapz);
-                }
-                else {
-                    glVertex3f(PQ[0].x, PQ[0].y, PQ[0].z);
-                    glVertex3f(PQ[1].x, PQ[1].y, PQ[1].z);
-                }
+                 if(viewdim==3)
+                     if(changePlotdim){
+                         glVertex3f(PQ[0].x+NQ[0].x*xf*rapz, PQ[0].y+NQ[0].y*xf*rapz, PQ[0].z+NQ[0].z*xf*rapz);
+                         glVertex3f(PQ[1].x+NQ[1].x*xf*rapz, PQ[1].y+NQ[1].y*xf*rapz, PQ[1].z+NQ[1].z*xf*rapz);
+                     }
+                     else {
+                         glVertex3f(PQ[0].x, PQ[0].y, PQ[0].z);
+                         glVertex3f(PQ[1].x, PQ[1].y, PQ[1].z);
+                     }
+                     else if(viewdim==2) {
+                         glVertex3f(PQ[0].x, PQ[0].y, xf*rapz);
+                         glVertex3f(PQ[1].x, PQ[1].y, xf*rapz);
+                     }
             }
         }
     }
