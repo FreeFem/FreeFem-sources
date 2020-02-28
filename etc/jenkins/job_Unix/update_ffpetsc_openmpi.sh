f5f416cb050e3e5747e8627924783ac8b16c03a2
Axel FOURMONT <axel.fourmont@inria.fr>
2020-02-28 14:02:47 +0100

    clean directory jenkins and add script to update

---
+++ etc/jenkins/job_Unix/update_ffpetsc_openmpi.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+## This script allows to install PETSc on vm2-2
+## See ./README.md
+
+echo "update ffPETSc(openmpi)"
+set -e
+
+# change default  compiler
+change_compiler=etc/jenkins/change_compiler/change_compiler-`uname -s`-`uname -r`-4_openmi.sh
+test -f "$change_compiler" && echo  source file "$change_compiler"
+test -f "$change_compiler" && cat  "$change_compiler"
+test -f "$change_compiler" && source "$change_compiler"
+
+if [ "$(uname)" == "Darwin" ]; then
+  # in case where the OS type is Darwin
+  PETSC_INSTALLDIR='/Users/Shared/openmpi/ff-petsc'
+elif [ "$(uname)" == "Linux" ]; then
+  # in case where the OS type is Linux
+PETSC_INSTALLDIR='/builds/Shared/openmpi/ff-petsc'
+fi
+
+# configuration & build
+autoreconf -i \
+  && ./configure --enable-download --prefix=$PETSC_INSTALLDIR --enable-bemtool=no \
+  && ./3rdparty/getall -a -o PETSc,Ipopt,NLopt,freeYams,FFTW,ARPACK,Gmm++,MMG3D,mshmet,MUMPS,htool \
+  && ./etc/jenkins/blob/build_PETSc.sh \
+  
+if [ $? -eq 0 ]
+then
+  echo "ffpetsc update complete"
+else
+  echo "ffpetsc update complete"
+  exit 1
+fi
+
