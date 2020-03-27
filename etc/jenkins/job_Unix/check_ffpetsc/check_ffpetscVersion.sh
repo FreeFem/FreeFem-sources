6513895530d0495cda046e739345d9576d94e99a
Axel FOURMONT <axel.fourmont@inria.fr>
2020-03-27 16:46:54 +0100

    correct script, bad path [ci skip]

--- etc/jenkins/job_Unix/check_ffpetsc/check_ffpetscVersion.sh
+++ etc/jenkins/job_Unix/check_ffpetsc/check_ffpetscVersion.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-releaseVersionffpetsc=$(grep "VERSION=" 3rdparty/ff-petsc/Makefile) | cut -c9-15
-installedVersionffpetsc=$(grep "VERSION="  ~/Shared/openmpi/ff-petsc/c/include/petscversion.h) | cut -c36-42 | cut -f1 -d "\""    #if version X.X.XX
 
+releaseVersionffpetsc=$(grep "VERSION=" 3rdparty/ff-petsc/Makefile) | cut -c9-15
+
+if [ "$(uname)" == "Darwin" ]; then
+  # in case where the OS type is Darwin
+  installedVersionffpetsc=$(grep "VERSION=" /Users/Shared/openmpi/ff-petsc/c/include/petscversion.h) | cut -c36-42 | cut -f1 -d "\""    #if version X.X.XX
+elif [ "$(uname)" == "Linux" ]; then
+  # in case where the OS type is Linux
+  installedVersionffpetsc=$(grep "VERSION=" /builds/Shared/openmpi/ff-petsc/c/include/petscversion.h) | cut -c36-42 | cut -f1 -d "\""    #if version X.X.XX
+fi
+#  check the version
 if [ "$releaseVersionffpetsc" == "$installedVersionffpetsc" ]; then
 	echo "installed release version PETSc is up to date"
 	updatescript=0	
@@ -10,8 +18,6 @@ else
 	echo "installed release version PETSc will be upgrated"
 	updatescript=1
 fi
-
-
 # change default  compiler load in update_ffpetsc_*.sh
 test $updatescript -gt 1 \
 	&& echo "************* upgrading openmpi ffpetsc  *************" \
