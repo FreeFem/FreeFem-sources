a19311c76d1259cc1adde62ab089c726ecdb0b76
Axel FOURMONT <axel.fourmont@inria.fr>
2020-03-30 11:34:07 +0200

    correct script [ci skip]

--- etc/jenkins/job_Unix/check_ffpetsc/check_ffpetscVersion.sh
+++ etc/jenkins/job_Unix/check_ffpetsc/check_ffpetscVersion.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-
-releaseVersionffpetsc=$(grep "VERSION=" 3rdparty/ff-petsc/Makefile) | cut -c9-15
+releaseVersionffpetsc=$(grep "VERSION=" 3rdparty/ff-petsc/Makefile | cut -c9-15)
+echo "release Version ffpetsc" $releaseVersionffpetsc
 
 if [ "$(uname)" == "Darwin" ]; then
   # in case where the OS type is Darwin
-  installedVersionffpetsc=$(grep "VERSION=" /Users/Shared/openmpi/ff-petsc/c/include/petscversion.h) | cut -c36-42 | cut -f1 -d "\""    #if version X.X.XX
+  installedVersionffpetsc=$(grep "VERSION=" /Users/Shared/openmpi/ff-petsc/c/include/petscversion.h | cut -c36-42 | cut -f1 -d "\"" )   #if version X.X.XX
 elif [ "$(uname)" == "Linux" ]; then
   # in case where the OS type is Linux
-  installedVersionffpetsc=$(grep "VERSION=" /builds/Shared/openmpi/ff-petsc/c/include/petscversion.h) | cut -c36-42 | cut -f1 -d "\""    #if version X.X.XX
+  installedVersionffpetsc=$(grep "VERSION=" /builds/Shared/openmpi/ff-petsc/c/include/petscversion.h | cut -c36-42 | cut -f1 -d "\"" )   #if version X.X.XX
 fi
+echo "installed Version ffpetsc" $installedVersionffpetsc
+
 #  check the version
 if [ "$releaseVersionffpetsc" == "$installedVersionffpetsc" ]; then
-	echo "installed release version PETSc is up to date"
+        echo "installed release version PETSc is up to date"
 else
-	# change default  compiler load in update_ffpetsc_*.sh
-	echo "installed release version PETSc will be upgrated"
-	echo "************* upgrading openmpi ffpetsc  *************" \
-	./etc/jenkins/job_Unix/check_ffpetsc/update_ffpetsc_openmpi.sh \
-	echo " ************* upgrading mpich ffpetsc success *************" \
-	cd 3rdparty/ff-petsc/ && make -j4 clean && cd ../.. \
-	echo " ************* upgrading mpich ffpetsc  *************" \
+        # change default  compiler load in update_ffpetsc_*.sh
+        echo "installed release version PETSc will be upgrated"
+        echo "************* upgrading openmpi ffpetsc  *************" \
+        ./etc/jenkins/job_Unix/check_ffpetsc/update_ffpetsc_openmpi.sh \
+        echo " ************* upgrading mpich ffpetsc success *************" \
+        cd 3rdparty/ff-petsc/ && make -j4 clean && cd ../.. \
+        echo " ************* upgrading mpich ffpetsc  *************" \
     ./etc/jenkins/job_Unix/check_ffpetsc/update_ffpetsc_mpich.sh \
-	echo " ************* upgrading mpich ffpetsc success *************"
+        echo " ************* upgrading mpich ffpetsc success *************"
 fi
-
 
