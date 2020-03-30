6e0a5b13ab6c82d12e4b7b12ffc3eb6bcd2cf6a1
Axel FOURMONT <axel.fourmont@inria.fr>
2020-03-30 13:05:54 +0200

    up and correct script ti install ffpetsc on VM2-2 [ci skip]

--- etc/jenkins/job_Unix/check_ffpetsc/check_ffpetscVersion.sh
+++ etc/jenkins/job_Unix/check_ffpetsc/check_ffpetscVersion.sh
@@ -3,27 +3,46 @@
 releaseVersionffpetsc=$(grep "VERSION=" 3rdparty/ff-petsc/Makefile | cut -c9-15)
 echo "release Version ffpetsc" $releaseVersionffpetsc
 
+# compilation with openmpi
 if [ "$(uname)" == "Darwin" ]; then
   # in case where the OS type is Darwin
-  installedVersionffpetsc=$(grep "VERSION_GIT" /Users/Shared/openmpi/ff-petsc/c/include/petscversion.h | cut -c36-42 | cut -f1 -d "\"" )   #if version X.X.XX
+  installedVersionffpetscO=$(grep "VERSION_GIT" /Users/Shared/openmpi/ff-petsc/c/include/petscversion.h | cut -c36-42 | cut -f1 -d "\"" )   #if version X.X.XX
 elif [ "$(uname)" == "Linux" ]; then
   # in case where the OS type is Linux
-  installedVersionffpetsc=$(grep "VERSION_GIT" /builds/Shared/openmpi/ff-petsc/c/include/petscversion.h | cut -c36-42 | cut -f1 -d "\"" )   #if version X.X.XX
+  installedVersionffpetscO=$(grep "VERSION_GIT" /builds/Shared/openmpi/ff-petsc/c/include/petscversion.h | cut -c36-42 | cut -f1 -d "\"" )   #if version X.X.XX
 fi
-echo "installed Version ffpetsc" $installedVersionffpetsc
+echo installed Version ffpetsc openmpi "$installedVersionffpetscO"
 
 #  check the version
-if [ "$releaseVersionffpetsc" == "$installedVersionffpetsc" ]; then
+if [ "$releaseVersionffpetsc" == "$installedVersionffpetscO" ]; then
         echo "installed release version PETSc is up to date"
 else
         # change default  compiler load in update_ffpetsc_*.sh
         echo "installed release version PETSc will be upgrated"
         echo "************* upgrading openmpi ffpetsc  *************" \
         && ./etc/jenkins/job_Unix/check_ffpetsc/update_ffpetsc_openmpi.sh \
-        echo " ************* upgrading mpich ffpetsc success *************" \
-        && cd 3rdparty/ff-petsc/ && make -j4 clean && cd ../.. \
-        echo " ************* upgrading mpich ffpetsc  *************" \
-        && ./etc/jenkins/job_Unix/check_ffpetsc/update_ffpetsc_mpich.sh \
-        echo " ************* upgrading mpich ffpetsc success *************"
+        echo " ************* upgrading openmpi ffpetsc success *************" \
+        && cd 3rdparty/ff-petsc/ && make -j4 clean && cd ../..
 fi
 
+
+# compilation with mpich
+if [ "$(uname)" == "Darwin" ]; then
+  # in case where the OS type is Darwin
+  installedVersionffpetscM=$(grep "VERSION_GIT" /Users/Shared/mpich/ff-petsc/c/include/petscversion.h | cut -c36-42 | cut -f1 -d "\"" )   #if version X.X.XX
+elif [ "$(uname)" == "Linux" ]; then
+  # in case where the OS type is Linux
+  installedVersionffpetscM=$(grep "VERSION_GIT" /builds/Shared/mpich/ff-petsc/c/include/petscversion.h | cut -c36-42 | cut -f1 -d "\"" )   #if version X.X.XX
+fi
+echo installed Version ffpetsc mpich "$installedVersionffpetscM"
+
+#  check the version
+if [ "$releaseVersionffpetsc" == "$installedVersionffpetscM" ]; then
+        echo "installed release version PETSc is up to date"
+else
+        # change default  compiler load in update_ffpetsc_*.sh
+        echo "installed release version PETSc will be upgrated"
+        echo "************* upgrading mpich ffpetsc  *************" \
+        && ./etc/jenkins/job_Unix/check_ffpetsc/update_ffpetsc_mpich.sh \
+        echo " ************* upgrading mpich ffpetsc success *************" \
+fi
