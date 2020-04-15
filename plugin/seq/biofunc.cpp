0639866e41a50f2db629efb83d765b147581cc50
Frederic Hecht <frederic.hecht@upmc.fr>
2020-04-15 11:22:23 +0200

    add un pluging biofunc (monod model) to array value.

--- plugin/seq/biofunc.cpp
+++ plugin/seq/biofunc.cpp
@@ -34,6 +34,54 @@ double fmonod(const double &xx, const double &k, const double &kb) {
   double x = max(xx, 0.);
   return (k * x) / (kb + x);
 }
+// on array ..
+KN<double> * fmonod( KN<double> * const&  pf , KN<double> *const &  px, const double &k, const double &kb)
+{
+    KN<double> &f = *pf, &xx=*px;
+    long n = f.N();
+    ffassert(n == xx.N());
+    for( int i=0; i<n; ++i)
+    {
+    double x = max(xx[i], 0.);
+    f[i]=  (k * x) / (kb + x);
+    }
+    return pf;
+}
+KN<double> * fmonod( KN<double> * const&  pf , KN<double> *const &  px, KN<double> *const &pk, const double &kb)
+{
+    KN<double> &f = *pf, &xx=*px , & k= *pk;
+    long n = f.N();
+    ffassert(n == xx.N() && n == k.N());
+    for( int i=0; i<n; ++i)
+    {
+        double x = max(xx[i], 0.);
+        f[i]=  (k[i] * x) / (kb + x);
+    }
+    return pf;
+}
+KN<double> * dfmonod( KN<double> * const&  pf , KN<double> *const &  px, const double &k, const double &kb)
+{
+    KN<double> &f = *pf, &xx=*px;
+    long n = f.N();
+    ffassert(n == xx.N());
+    for( int i=0; i<n; ++i)
+    {
+        double x = max(xx[i], 0.), a = kb + x;
+        f[i]=  k / a - k * x / (a * a);
+    }
+    return pf;
+}
+KN<double> * dfmonod( KN<double> * const&  pf , KN<double> *const &  px,KN<double> *const &pk, const double &kb)
+{
+    KN<double> &f = *pf, &xx=*px , & k= *pk;
+    long n = f.N();
+    ffassert(n == xx.N() && n == k.N());    for( int i=0; i<n; ++i)
+    {
+        double x = max(xx[i], 0.), a = kb + x;
+        f[i]=  k[i] / a - k[i] * x / (a * a);
+    }
+    return pf;
+}
 double dfmonod(const double &xx, const double &k, const double &kb) {
   double x = max(xx, 0.), a = kb + x;
   return k / a - k * x / (a * a);
@@ -48,6 +96,10 @@ double dfmonod(const double &xx, const double &kb) {
 }
 
 static void init( ) {
+  Global.Add("fmonod", "(", new OneOperator4_< KN<double> *, KN<double> *, KN<double> *, R, R >(fmonod));
+  Global.Add("fmonod", "(", new OneOperator4_< KN<double> *, KN<double> *, KN<double> *, KN<double> *, R >(fmonod));
+  Global.Add("dfmonod", "(", new OneOperator4_< KN<double> *, KN<double> *, KN<double> *, R, R >(dfmonod));
+  Global.Add("dfmonod", "(", new OneOperator4_< KN<double> *, KN<double> *, KN<double> *, KN<double> *, R >(dfmonod));
   Global.Add("fmonod", "(", new OneOperator3_< R, R, R, R >(fmonod));
   Global.Add("dfmonod", "(", new OneOperator3_< R, R, R, R >(dfmonod));
   Global.Add("fmonod", "(", new OneOperator2_< R, R, R >(fmonod));
