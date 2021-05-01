3469394c134c64133b8154ead97538965359dcc1
Pierre Jolivet <pierre@joliv.et>
2021-05-01 10:36:45 +0200

    Compilation failure...
    [ci skip]

--- plugin/mpi/SLEPc-complex.cpp
+++ plugin/mpi/SLEPc-complex.cpp
@@ -1,6 +1,8 @@
 //ff-c++-LIBRARY-dep: cxx11 slepccomplex petsccomplex [mkl|blas] hpddm mpi
 //ff-c++-cpp-dep:
 
+#include "ff++.hpp"
+
 static void Load_Init() {
     if (mpirank == 0)
         cerr << " ++ WARNING: SLEPc-complex has been superseded by PETSc-complex" << endl;
--- plugin/mpi/SLEPc.cpp
+++ plugin/mpi/SLEPc.cpp
@@ -1,6 +1,8 @@
 //ff-c++-LIBRARY-dep: cxx11 slepc petsc [mkl|blas] hpddm mpi
 //ff-c++-cpp-dep:
 
+#include "ff++.hpp"
+
 static void Load_Init() {
     if (mpirank == 0)
         cerr << " ++ WARNING: SLEPc has been superseded by PETSc" << endl;
