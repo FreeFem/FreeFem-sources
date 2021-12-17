8e1c7d103ec46bfa647a5d066d6df03a6a34218d
Gang Zhao <31978442+zhaog6@users.noreply.github.com>
2021-12-17 16:18:25 +0800

    Fixed missing character in dirname (#214)

--- plugin/seq/shell.cpp
+++ plugin/seq/shell.cpp
@@ -211,12 +211,12 @@ string dirname(const string *ppath) {
     }
   }
 
-  if (i == 0) {
+  if (i == -1) {
     return ".";
-  } else if (i == 1) {
+  } else if (i == 0) {
     return "/";
   } else {
-    return path.substr(0, i - 1);
+    return path.substr(0, i);
   }
 }
 
