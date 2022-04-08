b08ffcbadc5d4d0c7ef95f7e52129431b47b6b4b
Simon Garnotel <simon.garnotel@gmail.com>
2022-04-08 11:19:16 +0200

    deploy script minor fix

--- etc/jenkins/deployRelease/deployDEB.sh
+++ etc/jenkins/deployRelease/deployDEB.sh
@@ -40,7 +40,6 @@ touch "$DEB_NAME/DEBIAN/control"
 	echo "Description: FreeFEM, Finite Element Language software";
 	echo "Homepage: https://freefem.org";
 } >> "$DEB_NAME/DEBIAN/control"
-mkdir -p "$DEB_NAME/usr/local"
 mkdir -p "$DEB_NAME/usr/local/share/FreeFEM"
 mkdir -p "$DEB_NAME/usr/local/bin"
 mkdir -p "$DEB_NAME/usr/local/lib/ff++"
@@ -63,6 +62,12 @@ cp -r "$BUILD_DIR/bin/ffmaster" "$DEB_NAME/usr/local/bin/ffmaster"
 cp -r "$BUILD_DIR/bin/ffmedit" "$DEB_NAME/usr/local/bin/ffmedit"
 cp AUTHORS "$DEB_NAME/usr/share/doc/freefem/AUTHOR"
 cp README.md "$DEB_NAME/usr/share/doc/freefem/README.md"
+
+{
+	echo "loadpath += \"./\"";
+	echo "loadpath += \"/usr/local/lib/ff++/$VERSION/lib\"";
+	echo "includepath += \"/usr/local/lib/ff++/$VERSION/idp\"";
+}
 
 dpkg-deb --build "$DEB_NAME/"
 mv "$DEB_NAME.deb" "$GH_DEB_NAME"
