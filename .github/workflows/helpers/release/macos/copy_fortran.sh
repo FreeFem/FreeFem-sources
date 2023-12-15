ad434d7d26c40de07c576df662d4b7b227efaad5
Simon Garnotel <simon.garnotel@gmail.com>
2023-12-15 10:08:55 +0100

    MacOS release

---
+++ .github/workflows/helpers/release/macos/copy_fortran.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+PREFIX=$1
+
+# Create path
+mkdir -p "$PREFIX/gnu"
+
+# Fortran check (get libraries)
+FORTRAN_CHECK="$HOME/fortran-check"
+mkdir "$FORTRAN_CHECK"
+printf "program sizeofint\n  integer i\nend" >"$FORTRAN_CHECK/check.f90"
+gfortran "$FORTRAN_CHECK/check.f90" -o "$FORTRAN_CHECK/check"
+(ldd "$FORTRAN_CHECK/check") 2>&1 | awk '{print $NF}' | egrep -v '/usr/lib | "$FORTRAN_CHECK/check" | grep .dylib' >$PREFIX/gnu/list-dylib-gfortran
+rm -rf "$FORTRAN_CHECK"
+
+# Copy librairies
+BREW_LIB_GFORTRAN=$(grep gfortran "$PREFIX/gnu/list-dylib-gfortran")
+
+cd $(dirname "$BREW_LIB_GFORTRAN")
+cp -f libgfortran.*.dylib libquadmath.*.dylib libgcc_s.*.dylib "$PREFIX/gnu/"
+
+cd "$PREFIX/gnu"
+ln -sf libgfortran.?.dylib libgfortran.dylib
+ln -sf libquadmath.?.dylib libquadmath.dylib
