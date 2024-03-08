d2c53f19b6a6eab28c3e37e0c2ce3a40e94365ba
Simon Garnotel <simon.garnotel@gmail.com>
2024-03-08 16:39:02 +0100

    homebrew new path

--- etc/actions/macos/link_fortran.sh
+++ etc/actions/macos/link_fortran.sh
@@ -3,10 +3,17 @@
 set -e
 set -x
 
-sudo ln -fs /usr/local/bin/gfortran-12 /usr/local/bin/gfortran
-sudo ln -fs /usr/local/bin/gcc-12 /usr/local/bin/gcc
-sudo ln -fs /usr/local/bin/g++-12 /usr/local/bin/g++
+ARCH=$1
+
+LOCAL_PATH=/usr/local
+if [ "$ARCH" = "ARM64" ]; then
+	LOCAL_PATH=/opt/homebrew
+fi
+
+sudo ln -fs "$LOCAL_PATH/bin/gfortran-12" "$LOCAL_PATH/bin/gfortran"
+sudo ln -fs "$LOCAL_PATH/bin/gcc-12" "$LOCAL_PATH/bin/gcc"
+sudo ln -fs "$LOCAL_PATH/bin/g++-12" "$LOCAL_PATH/bin/g++"
 
 # symlink dylib location for previous versions
-sudo ln -fs /usr/local/opt/gcc/lib/gcc/12 /usr/local/opt/gcc/lib/gcc/11
-sudo ln -fs /usr/local/opt/gcc/lib/gcc/12 /usr/local/opt/gcc/lib/gcc/10
+sudo ln -fs "$LOCAL_PATH/opt/gcc/lib/gcc/12" "$LOCAL_PATH/opt/gcc/lib/gcc/11"
+sudo ln -fs "$LOCAL_PATH/opt/gcc/lib/gcc/12" "$LOCAL_PATH/opt/gcc/lib/gcc/10"
