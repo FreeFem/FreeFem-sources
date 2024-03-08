8f2410afe72e97867d7b8d5df7bca2517f1b4bf4
Simon Garnotel <simon.garnotel@gmail.com>
2024-03-08 14:35:59 +0100

    readme

--- etc/actions/release/macos/create_dmg.sh
+++ etc/actions/release/macos/create_dmg.sh
@@ -5,9 +5,12 @@ set -x
 
 PACKAGE_DIR=$1
 DMG_BASENAME=$2
+VERSION=$3
 
 mkdir "$HOME/$DMG_BASENAME"
 mv "$PACKAGE_DIR/Applications/FreeFem++.app" "$HOME/$DMG_BASENAME"
 cp ./bin/script/Install-app.sh "$HOME/$DMG_BASENAME"
+FFTESTDIR=$(find . -name Laplace.edp)
+sed -e "s/@VV@/$VERSION/" -e "s%@APPFF@%$DMG_BASENAME%" -e s%@FFTEST@%$FFTESTDIR% <./bin/script/README.md.in >"$HOME/$DMG_BASENAME/README.md"
 
 hdiutil create -srcfolder "$HOME/$DMG_BASENAME" -fs HFS+ "$DMG_BASENAME.dmg"
--- etc/actions/release/macos/fix_fortran.sh
+++ etc/actions/release/macos/fix_fortran.sh
@@ -11,10 +11,4 @@ FORTRAN_FIX="$HOME/fortran-fix"
 mkdir "$FORTRAN_FIX"
 find "$PACKAGE_DIR/$PREFIX" -name '*.dylib' >"$FORTRAN_FIX/dylib-ff.txt"
 
-# LIB_FORTRAN=$(grep gfortran $PREFIX/gnu/list-dylib-gfortran)
-# LIB_QUADMATH=$(grep libquadmath $PREFIX/gnu/list-dylib-gfortran)
-# LIB_FORTRAN_OLD_DIR=$(dirname "$LIB_FORTRAN")
-# LIB_FORTRAN_DYLIB=$(basename "$LIB_FORTRAN")
-# LIB_QUADMATH_DYLIB=$(basename "$LIB_QUADMATH")
-
 ./bin/change-dylib-gfortran-2 "$PREFIX/gnu" $(cat "$FORTRAN_FIX/dylib-ff.txt")
