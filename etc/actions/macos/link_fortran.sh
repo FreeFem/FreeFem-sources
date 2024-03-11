d184ccb4ccf11c75f7f8bebb6065e87204ab6ea2
Simon Garnotel <simon.garnotel@gmail.com>
2024-03-11 14:06:35 +0100

    gcc13 & msmpi

--- etc/actions/macos/link_fortran.sh
+++ etc/actions/macos/link_fortran.sh
@@ -10,10 +10,11 @@ if [ "$ARCH" = "ARM64" ]; then
 	LOCAL_PATH=/opt/homebrew
 fi
 
-sudo ln -fs "$LOCAL_PATH/bin/gfortran-12" "$LOCAL_PATH/bin/gfortran"
-sudo ln -fs "$LOCAL_PATH/bin/gcc-12" "$LOCAL_PATH/bin/gcc"
-sudo ln -fs "$LOCAL_PATH/bin/g++-12" "$LOCAL_PATH/bin/g++"
+sudo ln -fs "$LOCAL_PATH/bin/gfortran-13" "$LOCAL_PATH/bin/gfortran"
+sudo ln -fs "$LOCAL_PATH/bin/gcc-13" "$LOCAL_PATH/bin/gcc"
+sudo ln -fs "$LOCAL_PATH/bin/g++-13" "$LOCAL_PATH/bin/g++"
 
 # symlink dylib location for previous versions
-sudo ln -fs "$LOCAL_PATH/opt/gcc/lib/gcc/12" "$LOCAL_PATH/opt/gcc/lib/gcc/11"
-sudo ln -fs "$LOCAL_PATH/opt/gcc/lib/gcc/12" "$LOCAL_PATH/opt/gcc/lib/gcc/10"
+sudo ln -fs "$LOCAL_PATH/opt/gcc/lib/gcc/13" "$LOCAL_PATH/opt/gcc/lib/gcc/12"
+sudo ln -fs "$LOCAL_PATH/opt/gcc/lib/gcc/13" "$LOCAL_PATH/opt/gcc/lib/gcc/11"
+sudo ln -fs "$LOCAL_PATH/opt/gcc/lib/gcc/13" "$LOCAL_PATH/opt/gcc/lib/gcc/10"
