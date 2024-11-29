b012dbcdec707b6da97cc434ce44bfc2de7ff5a2
Frederic Hecht <frederic.hecht@upmc.fr>
2024-11-29 10:51:37 +0100

    add the build of .edp file form .md files

--- etc/tools/md2edp.cpp
+++ etc/tools/md2edp.cpp
@@ -14,31 +14,40 @@ int  md2edp(const char *fname)
     cout << " md2edp " << fn << " -> " << ofn << endl;
     
     ifstream fin(fn.c_str());
-    ofstream fout(ofn.c_str());
+    ofstream *fout=0;
+    
     //
     long cas = 1;
-    if( fin && fout)
+    if( fin )
     {
-        fout << "//  created with md2edp " << fn << endl;
+        
         while (!fin.eof())
         {
             string ln ;
             getline(fin,ln);
             if(ln.find("~~~freefem")==0) cas++;
             else if ((cas%2 == 0 ) && (ln.find("~~~")==0))  cas++;
-            else if( cas%2==0) fout << ln << endl;
+            else if( cas%2==0) { 
+                if(!fout) { fout = new ofstream(ofn.c_str()); 
+                if(!fout) break; 
+                *fout << "//  created with md2edp " << fn << endl;
+                }
+                *fout << ln << endl;}
+
           //  cout << cas << " ..." << ln << endl;
             
         }
     } else {
         ok=1;cout << " error of file (skip) "<< fname << endl;
     }
+    if( !fout) cout << " skip "<< fname << " no freefem code " << endl;
+    if(fout) delete fout;
     return ok;
 }
 int main(int argc,const char ** argv)
 {
     int ok =0;
     for(size_t i=1; i< argc;i++)
-        if(!md2edp(argv[i])) ok =1;
+        if(md2edp(argv[i])) ok =1;
     return ok;
 }
